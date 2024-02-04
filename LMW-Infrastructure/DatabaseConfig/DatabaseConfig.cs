629e0682af9401af6c21e772bf163232ad4d84db
LewisWhittard <63914200+LewisWhittard@users.noreply.github.com>
2024-02-04 12:21:38 +0000

    LMWP-2 Entity Framework Set up

---
+++ LMW-Infrastructure/DatabaseConfig/DatabaseConfig.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System;
+using LMW_Infrastructure.Model.ContactForm;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.Identity.Client;
+
+public class ApplicationContext : DbContext
+{
+	public DbSet<IContactForm> ContactForm { get; set; }
+	public DbSet<Icontent> Content { get; set; }
+	public DbSet<IJsonLD> JsonLDs { get; set; }
+	public DbSet<IMicroData> MicroData { get; set; }
+	public DbSet<IWebPage> WebPages { get; set; }
+
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+		optionsBuilder.UseSqlServer("Server={server};Database={Database};User Id={UserId};Password={Password};");
+	}
+}
---
+++ LMW-Infrastructure/Model/JsonLD/Interface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMW_Infrastructure.Model.ContactForm
+{
+	public interface IJsonLD
+	{
+		public int ID { get; set; }
+		public bool Inactive { get; set; }
+		public bool Deleted { get; set; }
+	}
+}
