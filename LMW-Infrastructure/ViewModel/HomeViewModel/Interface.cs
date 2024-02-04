7e022dd35353e3c7e1991c2a20f3c26bd00639e1
LewisWhittard <63914200+LewisWhittard@users.noreply.github.com>
2024-02-04 20:52:34 +0000

    Inroduce Home ViewModel

---
+++ LMW-Infrastructure/ViewModel/HomeViewModel/Interface.cs
@@ -0,0 +1,14 @@
+﻿using LMW_Infrastructure.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMW_Infrastructure.ViewModel
+{
+	public interface IHomePageViewModel
+	{
+
+	}
+}
