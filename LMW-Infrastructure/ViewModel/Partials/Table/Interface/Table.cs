4f380d4e9fd3499475f4f77c5caec2a98dfbb823
LewisWhittard <63914200+LewisWhittard@users.noreply.github.com>
2024-02-18 13:15:27 +0000

    LMWP - 10 add interface to partial

---
+++ LMW-Infrastructure/ViewModel/Partials/Table/Interface/Table.cs
@@ -0,0 +1,19 @@
+﻿using LMW_Infrastructure.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMW_Infrastructure.ViewModel.Partials.Table.Interface
+{
+    public interface ITable
+    {
+        string Title { get; set; }
+        string Headers { get; set; }
+        string Values { get; set; }
+        string GetTitle();
+        string GetColumnHeaders();
+        string GetColumnValues();
+    }
+}
--- LMW-Infrastructure/ViewModel/Partials/Table/Table.cs
+++ LMW-Infrastructure/ViewModel/Partials/Table/Table.cs
@@ -1,17 +1,18 @@
 ﻿using LMW_Infrastructure.Model;
+using LMW_Infrastructure.ViewModel.Partials.Table.Interface;
 
 namespace LMWDev.Views.Partials
 {
-    public class Table
+    public class Table : ITable
     {
         public string Title { get; set; }
         public string Headers { get; set; }
         public string Values { get; set; }
-        private readonly Content _Content;
+        private readonly Content _content;
 
         public Table(Content content)
         {
-            _Content = content;
+            _content = content;
             Title = GetTitle();
             Headers = GetColumnHeaders();
             Values = GetColumnValues();
@@ -19,23 +20,23 @@ namespace LMWDev.Views.Partials
 
         public string GetTitle()
         {
-            var result = _Content.Components.Where(x => x.ContentComponentType == ContentComponentType.Title).FirstOrDefault().Value;
+            var result = _content.Components.Where(x => x.ContentComponentType == ContentComponentType.Title).FirstOrDefault().Value;
             return result;
         }
 
         public string GetColumnHeaders()
         {
-            var result = _Content.Components
+            var result = _content.Components
             .Where(x => x.ContentComponentType == ContentComponentType.Header)
             .OrderBy(x => x.DisplayOrder)
-            .Select(x => $"<tr>{x.HMTLContentComponentType}{x.Value}{x.HMTLContentComponentType}</tr>")
+            .Select(x => $"<tr>{x.HMTLContentComponentType.ToString()}{x.Value}{"/" + x.HMTLContentComponentType.ToString()}</tr>")
             .Aggregate((current, next) => current + next);
             return result;
         }
 
         public string GetColumnValues()
         {
-            var result = _Content.Components
+            var result = _content.Components
             .Where(x => x.ContentComponentType == ContentComponentType.Content)
             .OrderBy(x => x.DisplayOrder)
             .Select(x => $"<tr>{x.HMTLContentComponentType}{x.Value}{x.HMTLContentComponentType}</tr>")
