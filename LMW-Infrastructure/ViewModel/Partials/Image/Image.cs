2d0d707b7e86c891e46e10d36091fe26985b9377
LewisWhittard <63914200+LewisWhittard@users.noreply.github.com>
2024-02-18 15:53:14 +0000

    LMWP-10 Image Interface

---
+++ LMW-Infrastructure/ViewModel/Partials/Image/Image.cs
@@ -0,0 +1,11 @@
+﻿using LMW_Infrastructure.ViewModel.Partials.Image.Interface;
+
+namespace LMW_Infrastructure.ViewModel.Partials.Image
+{
+    public class Image : IImage
+    {
+        public string Value { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string? ItemProp { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string? Alt { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
---
+++ LMW-Infrastructure/ViewModel/Partials/Image/Interface/Image.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMW_Infrastructure.ViewModel.Partials.Image.Interface
+{
+    public interface IImage
+    {
+        public string Value { get; set; }
+        public string? ItemProp { get; set; }
+        public string? Alt {  get; set; }
+    }
+}
