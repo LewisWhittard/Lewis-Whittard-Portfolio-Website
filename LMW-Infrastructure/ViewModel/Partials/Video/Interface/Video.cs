384d6afd1028fab75fd22e3c5489480ca25b38b1
LewisWhittard <63914200+LewisWhittard@users.noreply.github.com>
2024-02-19 21:50:16 +0000

    LMWP-10 video interface and partial class

---
+++ LMW-Infrastructure/ViewModel/Partials/Video/Interface/Video.cs
@@ -0,0 +1,12 @@
+﻿using LMW_Infrastructure.Model;
+
+namespace LMW_Infrastructure.ViewModel.Partials.Video.Interface
+{
+    public interface IVideo
+    {
+        public string Value { get; set; }
+        public HTMLContentComponentType HTMLContentComponentType { get; set; }
+        string PopulateValue();
+        HTMLContentComponentType PopulateHTMLContentComponentType();
+    }
+}
---
+++ LMW-Infrastructure/ViewModel/Partials/Video/Video.cs
@@ -0,0 +1,32 @@
+﻿using LMW_Infrastructure.Model;
+using LMW_Infrastructure.ViewModel.Partials.Video.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMW_Infrastructure.ViewModel.Partials.Video
+{
+    public class Video : IVideo
+    {
+        public string Value { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public HTMLContentComponentType HTMLContentComponentType { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public HTMLContentComponentType PopulateHTMLContentComponentType()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string? PopulateItemProp()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string PopulateValue()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+}
