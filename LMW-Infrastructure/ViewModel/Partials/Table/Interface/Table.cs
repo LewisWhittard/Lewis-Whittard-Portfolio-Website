7beba4c58519fc08477d28921b1ee605c55fa1b9
LewisWhittard <63914200+LewisWhittard@users.noreply.github.com>
2024-02-19 21:54:13 +0000

    LMWP-10 tidy up name spaces

--- LMW-Infrastructure/ViewModel/Partials/Table/Interface/Table.cs
+++ LMW-Infrastructure/ViewModel/Partials/Table/Interface/Table.cs
@@ -1,11 +1,4 @@
-﻿using LMW_Infrastructure.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LMW_Infrastructure.ViewModel.Partials.Table.Interface
+﻿namespace LMW_Infrastructure.ViewModel.Partials.Table.Interface
 {
     public interface ITable
     {
--- LMW-Infrastructure/ViewModel/Partials/Text/Interface/Text.cs
+++ LMW-Infrastructure/ViewModel/Partials/Text/Interface/Text.cs
@@ -1,9 +1,4 @@
 ﻿using LMW_Infrastructure.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LMW_Infrastructure.ViewModel.Partials.Text.Interface
 {
