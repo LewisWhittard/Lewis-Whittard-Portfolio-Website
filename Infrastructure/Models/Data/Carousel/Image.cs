023da3fb980e076cdc8f0dca89beed7b8cbd0758
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-03-14 08:54:14 +0000

    LMWD-15 Image interfaces carousel

---
+++ Infrastructure/Models/Data/Carousel/Image.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure.Models.Data.Carousel.Interfaces;
+using Infrastructure.Models.Data.Interface;
+
+namespace Infrastructure.Models.Data.Carousel
+{
+    public class Image : IImage, IData
+    {
+        public string Source { get; set; }
+        public int DisplayOrder { get; set; }
+        public int Id { get; set; }
+        public bool Deleted { get; set; }
+        public bool Inactive { get; set; }
+    }
+}
