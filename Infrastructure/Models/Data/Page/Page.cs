fa570fe95bde8eb1950e5169fa25f348f5ec1e68
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-03-15 22:24:07 +0000

    LMWD-15 IPage

--- Infrastructure/Models/Data/Page/Page.cs
+++ Infrastructure/Models/Data/Page/Page.cs
@@ -1,6 +1,9 @@
-﻿namespace Infrastructure.Models.Data.Page.Interface
+﻿using Infrastructure.Models.Data.Interface;
+using Infrastructure.Models.Data.Page.Interface;
+
+namespace Infrastructure.Models.Data.Page
 {
-    public interface IPage
+    public class Page : IPage,IData
     {
         public string Webpage { get; set; }
         public List<Card.Card> Cards { get; set; }
@@ -8,5 +11,8 @@
         public List<CarouselCard.Card> CarouselCards { get; set; }
         public List<InfomationBlock.InfomatonBlock> InfomationBlocks { get; set; }
         public List<Table.Table> Tables { get; set; }
+        public int Id { get; set; }
+        public bool Deleted { get; set; }
+        public bool Inactive { get; set; }
     }
 }
