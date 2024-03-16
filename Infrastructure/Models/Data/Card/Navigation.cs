538afc4d1f9bb51ed436fb19663b81b00f63bd49
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-03-16 15:29:24 +0000

    Revert "LMWD-15 Tidy up"
    This reverts commit 70b62d820762908f5ed9fabd382c3f3506bb800b.

--- Infrastructure/Models/Data/Card/Navigation.cs
+++ Infrastructure/Models/Data/Card/Navigation.cs
@@ -5,6 +5,7 @@ namespace Infrastructure.Models.Data.Card
 {
     public class Navigation : IData, INavigation
     {
+        
         public int Id { get; set; }
         public bool Deleted { get; set; }
         public bool Inactive { get; set; }
