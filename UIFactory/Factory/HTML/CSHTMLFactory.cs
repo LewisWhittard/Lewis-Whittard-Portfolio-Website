3bb772264d6f018b0ec48e977faef1aa6bffec09
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-03-18 21:22:59 +0000

    LMWD-15 IData Cast

--- UIFactory/Factory/HTML/CSHTMLFactory.cs
+++ UIFactory/Factory/HTML/CSHTMLFactory.cs
@@ -3,15 +3,18 @@ using UIFactory.Concreate.CSHTML.Carousel;
 using UIFactory.Concreate.CSHTML.CarouselCard;
 using UIFactory.Concreate.CSHTML.InfomationBlock;
 using UIFactory.Concreate.CSHTML.Table;
+using Infrastructure.Models.Data.Interface;
 
 namespace UIFactory.Factory.HTML
 {
-    class CSHTMLFactory
+    class CSHTMLFactory<T>
     {
         // Factory method to create HMTL
-        public IHTML CreateCSHMTL(IHTML type)
+        public IHTML CreateCSHMTL(IData type)
         {
-            switch (type.Name)
+            var value = CastFromIDataToClass(type);
+
+            switch (value)
             {
                 case nameof(Card):
                     return new Concreate.CSHTML.Card.Card();
@@ -27,6 +30,26 @@ namespace UIFactory.Factory.HTML
                     throw new ArgumentException("Unknown type: " + type);
             }
         }
+
+        public object CastFromIDataToClass(IData value)
+        {
+            var type = value.GetType();
+
+            switch (type.Name)
+            {
+                case nameof(Infrastructure.Models.Data.Card.Card):
+                    return (Infrastructure.Models.Data.Card.Card)value;
+                case nameof(Infrastructure.Models.Data.Carousel.Carousel):
+                    return (Infrastructure.Models.Data.Carousel.Carousel)value;
+                case nameof(Infrastructure.Models.Data.CarouselCard.CarouselCard):
+                    return (Infrastructure.Models.Data.CarouselCard.CarouselCard)value;
+                case nameof(Infrastructure.Models.Data.InfomationBlock.InfomatonBlock):
+                    return (Infrastructure.Models.Data.InfomationBlock.InfomatonBlock)value;
+                case nameof(Infrastructure.Models.Data.Table.Table):
+                    return(Table)value;
+                default:
+                    throw new ArgumentException("Unknown type: " + type);
+            }
         }
     }
 }
