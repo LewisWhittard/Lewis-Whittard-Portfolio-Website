1b80e40f7da6c5748709069d3b364a56d39a7a90
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-03-31 20:49:03 +0100

    Video in infranstructure

--- Infrastructure/Models/Data/Interface/UIConcreate.cs
+++ Infrastructure/Models/Data/Interface/UIConcreate.cs
@@ -6,6 +6,7 @@
         Carousel = 1,
         CarouselCard = 2,
         InfomationBlock = 3,
+        Video = 4,
         Table  = 5
     }
 }
---
+++ Infrastructure/Models/Data/Video/Video.cs
@@ -0,0 +1,26 @@
+﻿using Infrastructure.Models.Data.Card.Interface;
+using Infrastructure.Models.Data.Interface;
+using Infrastructure.Models.Data.Video.Interfaces;
+using System.Text.Json.Serialization;
+
+namespace Infrastructure.Models.Data.Card
+{
+    public class Video : IVideo , IData
+    {
+        public string Source { get; set; }
+        public string Title {  get; set; }
+        public string Description { get; set; }
+        public string Navigation { get; set; }
+        public int Id { get; set; }
+        public bool Deleted { get; set; }
+        public bool Inactive { get; set; }
+        public int DisplayOrder { get; set; }
+        [JsonIgnore]
+        public UIConcreate? UIConcreateType { get; set; }
+
+        public Card()
+        {
+            UIConcreateType = UIConcreate.Video;
+        }
+    }
+}
