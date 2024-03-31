5cba446c8313b691f0ef3023e9dda954dce37fc0
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-03-31 22:01:29 +0100

    LWMD-15 Video concreate

---
+++ UIFactory/Factory/Concreate/CSHTML/Video/Video.cs
@@ -0,0 +1,26 @@
+﻿using Infrastructure.Models.Data.Interface;
+using UIFactory.Factory.Concreate.CSHTML.Interface;
+using UIFactory.Factory.Concreate.CSHTML.Video.Interfaces;
+
+namespace UIFactory.Factory.Concreate.CSHTML.Video
+{
+    public class Video : ICSHTML, IVideo
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Navigation { get; set; }
+        public int DisplayOrder { get; set; }
+        public string Source { get; set; }
+        private readonly Infrastructure.Models.Data.Video.Interfaces.IVideo _Video;
+
+        public Video(Infrastructure.Models.Data.Video.Interfaces.IVideo video)
+        {
+            _Video = video;
+            Title = _Video.Title;
+            Description = _Video.Description;
+            Navigation = _Video.Navigation;
+           DisplayOrder = _Video.DisplayOrder;
+            Source = _Video.Source;
+        }
+    }
+}
