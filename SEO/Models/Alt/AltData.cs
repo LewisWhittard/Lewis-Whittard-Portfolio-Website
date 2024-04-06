d5c276181e6060d97a1367e252638f7aa09bc8d7
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-04-06 15:51:28 +0100

    LWMD-15 Add Alt Data and interface

---
+++ SEO/Models/Alt/AltData.cs
@@ -0,0 +1,12 @@
+﻿using Infrastructure.Models.Data.Interface;
+using SEO.Models.Alt.Interface;
+
+namespace SEO.Models.Alt
+{
+    public class AltData : IAltData
+    {
+        public int DataId { get; set; }
+        public UIConcrete? UIConcreteType { get; set; }
+        public string Page { get; set; }
+    }
+}
---
+++ SEO/Models/Alt/Interface/IAltData.cs
@@ -0,0 +1,11 @@
+﻿using Infrastructure.Models.Data.Interface;
+
+namespace SEO.Models.Alt.Interface
+{
+    public interface IAltData
+    {
+        public int DataId { get; set; }
+        public UIConcrete? UIConcreteType { get; set; }
+        public string Page { get; set; }
+    }
+}
