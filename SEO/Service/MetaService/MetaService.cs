42d19555b16886c9ff0c30f8910ea80044e16711
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-04-16 21:09:34 +0100

    LMWD-16 introduce meta service

---
+++ SEO/Service/MetaService/MetaService.cs
@@ -0,0 +1,15 @@
+﻿using Infrastructure.Models.Data.Interface;
+using SEO.Models.JsonLD.Interface;
+using SEO.Models.Meta.Interface;
+using SEO.Service.MetaService.Interface;
+
+namespace SEO.Service.JsonLDService
+{
+    public class MetaService : IMetaService
+    {
+        public List<IMeta> GetBySuperClassGUID(IData data)
+        {
+            return new List<IMeta>();
+        }
+    }
+}
