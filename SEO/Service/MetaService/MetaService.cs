3c732001af52107626e9f8f990d1d48af15e3b9f
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-04-16 22:05:07 +0100

    LMWD-16  change to take page name

--- SEO/Service/MetaService/MetaService.cs
+++ SEO/Service/MetaService/MetaService.cs
@@ -1,13 +1,11 @@
-﻿using Infrastructure.Models.Data.Interface;
-using SEO.Models.JsonLD.Interface;
-using SEO.Models.Meta.Interface;
+﻿using SEO.Models.Meta.Interface;
 using SEO.Service.MetaService.Interface;
 
 namespace SEO.Service.JsonLDService
 {
     public class MetaService : IMetaService
     {
-        public List<IMetaData> GetByPageName(IData data)
+        public List<IMetaData> GetByPageName(string data)
         {
             return new List<IMetaData>();
         }
