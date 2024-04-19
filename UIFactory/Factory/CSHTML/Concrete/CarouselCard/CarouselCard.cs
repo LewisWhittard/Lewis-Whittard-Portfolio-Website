4fe30bcf19ac5bc4dbff5953af598cec64bb939f
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-04-19 22:08:36 +0100

    LWWD-16 return cards as rows

--- UIFactory/Factory/CSHTML/Concrete/CarouselCard/CarouselCard.cs
+++ UIFactory/Factory/CSHTML/Concrete/CarouselCard/CarouselCard.cs
@@ -31,6 +31,7 @@ namespace UIFactory.Factory.CSHTML.Concrete.CarouselCard
             if (cardCount < 9) 
             {
                 List<Card> cards = PopulateBlankCards(cardCount);
+                Cards.AddRange(cards);
             }
 
             DisplayOrder = _carouselCard.DisplayOrder;
@@ -50,6 +51,22 @@ namespace UIFactory.Factory.CSHTML.Concrete.CarouselCard
             }
             
             return blankCards;
+        }
+
+        public List<List<Card>> ReturnCardsAsRows()
+        {
+            var cards = Cards.OrderByDescending(x => x.DisplayOrder).ToList();
+
+            List<List<Card>> Result = new List<List<Card>>();
+            List<Card> rowOne = new List<Card> { cards[0], cards[1], cards[2] };
+            List<Card> rowTwo = new List<Card> { cards[3], cards[4], cards[5] };
+            List<Card> rowThree = new List<Card> { cards[6], cards[7], cards[8] };
+
+            Result.Add(rowOne);
+            Result.Add(rowTwo);
+            Result.Add(rowThree);
+
+            return Result;
         }
     }
 }
