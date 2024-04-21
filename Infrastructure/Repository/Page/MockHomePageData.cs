2da07e98c6d9fc6fa81b3671a35a4e73eec1d687
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-04-21 23:08:36 +0100

    LWMD-17 Cerifications Table

--- Infrastructure/Repository/Page/MockHomePageData.cs
+++ Infrastructure/Repository/Page/MockHomePageData.cs
@@ -10,7 +10,7 @@ namespace Infrastructure.Repository.Page
             Heading heading00 = new Heading(0,false,false,"About Me",0,0, "HomePageInfomationBlock0Heading0", 1);
             Paragraph paragraph00 = new Paragraph("Having over 5 years experience in the software industry I have worked as a software tester using mostly manual techniques and dabbled with automation. Currently I am transitioning into a support developer role.",1,0,false,false,0, "HomePageInfomationBlock0Paragraph0");
             Heading heading01 = new Heading(1, false, false, "Hobbies and Interests", 2, 0, "HomePageInfomationBlock0Heading1", 2);
-            Paragraph paragraph01 = new Paragraph("Having over 5 years experience in the software industry I have worked as a software tester using mostly manual techniques and dabbled with automation. Currently I am transitioning into a support developer role.", 4, 1, false, false, 0, "HomePageInfomationBlock0Paragraph1");
+            Paragraph paragraph01 = new Paragraph("I enjoy improving my skill set by doing side projects outside of my day job, which will be displayed on this portfolio site. Some of this stems for my passion for gaming which I have done from a young age. This helped ignite my interest in computing as a whole. I also do kick boxing for both health and enjoyment.", 4, 1, false, false, 0, "HomePageInfomationBlock0Paragraph1");
             Image image00 = new Image("LewisWhittard.png", 5,0,false,false, "HomePageInfomationBlock0Image0");
             List<Heading> headings0 = new List<Heading>() { heading00, heading01 };
             List<Paragraph> paragraphs0 = new List<Paragraph>() { paragraph00, paragraph01 };
@@ -34,7 +34,29 @@ namespace Infrastructure.Repository.Page
                 new List<Column>() { column00, column01, column02 },
                 new List<Column>() { column03, column04, column05 }
             };
-            Table table1 = new Table(0,false,false,1,headers0,columns0,"HomePage","HomePageTable0", "Qualifications");
+
+            Header header10 = new Header(3, false, false, 0, 1, "Certification", "HomePageTable1Header0");
+            Header header11 = new Header(4, false, false, 1, 1, "Result", "HomePageTable1Header1");
+            Header header12 = new Header(5, false, false, 2, 1, "Year", "HomePageTable1Header2");
+
+            Column column10 = new Column(6, false, false, "ISTQB® Certified Tester, Foundation Level (4.0)", 0, 0, 1, "HomePageTable1Column0");
+            Column column11 = new Column(7, false, false, "Pass", 1, 0, 1, "HomePageTable1Column1");
+            Column column12 = new Column(8, false, false, "2024", 2, 0, 1, "HomePageTable1Column2");
+            Column column13 = new Column(9, false, false, "BCS Level 3 Certificate in Programming (603/1192/7)", 0, 1, 1, "HomePageTable1Column3");
+            Column column14 = new Column(10, false, false, "Pass", 1, 1, 1, "HomePageTable1Column4");
+            Column column15 = new Column(11, false, false, "2019", 2, 1, 1, "HomePageTable1Column5");
+            Column column16 = new Column(12, false, false, "BCS Level 3 Certificate in Software Development Context and Methodologies (603/1191/5)", 0, 2, 1, "HomePageTable1Column6");
+            Column column17 = new Column(13, false, false, "Pass", 1, 2, 1, "HomePageTable1Column7");
+            Column column18 = new Column(14, false, false, "2019", 2, 2, 1, "HomePageTable1Column8");
+            List<Header> headers1 = new List<Header>() { header00, header01, header02 };
+            List<List<Column>> columns1 = new List<List<Column>>()
+            {
+                new List<Column>() { column10, column11, column12 },
+                new List<Column>() { column13, column14, column15 },
+                new List<Column>() { column16, column17, column18 }
+            };
+
+            Table table1 = new Table(0,false,false,1,headers1,columns1,"HomePage","HomePageTable0", "Certifications");
 
             List<Table> tables = new List<Table>()
             {
