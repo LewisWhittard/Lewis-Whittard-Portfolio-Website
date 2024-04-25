9f9c1a050651a3a266dfa02ae1affca2352df4ba
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-04-26 00:34:22 +0100

    LWMD-17 PageServiceTests

---
+++ InfrastructureTests/PageService.cs
@@ -0,0 +1,42 @@
+﻿using Infrastructure.Service.Page;
+using Infrastructure.Repository.Page;
+
+public class PageServiceTests
+{
+    [Theory]
+    [InlineData("HomePage")]
+    [InlineData("SecondPage")]
+    [InlineData("NoPage")]
+    public void GetByPageName_ReturnsPage(string pageName)
+    {
+        // Arrange
+        var mockRepository = new MockPageRepository();
+        var pageService = new PageService(mockRepository);
+
+        // Act
+        Infrastructure.Models.Data.Page.Page? page = pageService.GetByPageName(pageName);
+
+        // Assert
+        if (pageName != "NoPage")
+        {
+            Assert.Equal(pageName, page.PageName);
+        }
+        else
+        {
+            Assert.Null(page);
+        }
+    }
+
+    [Fact]
+    [InlineData("HomePage")]
+    public void GetByPageNameAsIDataList_ReturnsIDataList_WhenPageExists()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+
+        Assert.True(true);
+    }
+}
