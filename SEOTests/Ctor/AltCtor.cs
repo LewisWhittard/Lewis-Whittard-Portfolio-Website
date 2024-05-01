7b4611fd7fece38e82d818670c279362ddd969db
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-05-01 23:58:10 +0100

    LMWD-17 Alt CTOR Tests

--- SEOTests/Ctor/AltCtor.cs
+++ SEOTests/Ctor/AltCtor.cs
@@ -1,12 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Infrastructure.Models.Data.Interface;
+using SEO.Models.Alt;
 
 namespace SEOTests.Ctor
 {
-    internal class AltCtor
+    public class AltCtor
     {
+        [Theory]
+        [InlineData("123456", UIConcrete.Video, "HomePage", 1, "SomeValue", "789012", 1001, false, true)]
+        [InlineData("123456", null, "HomePage", 1, "SomeValue", "789012", 1001, false, true)]
+        public void MetaData_Constructor_SetsPropertiesCorrectly(string superClassGUID, UIConcrete? uiConcreteType, string page, int? displayOrder, string value, string guid, int id, bool deleted, bool inactive)
+        {
+            //arrange, Act
+            AltData altData = new AltData(superClassGUID, uiConcreteType, page, displayOrder, value, guid, id, deleted, inactive);
+
+            // Assert
+            Assert.Equal(superClassGUID, altData.SuperClassGUID);
+            Assert.Equal(uiConcreteType, altData.UIConcreteType);
+            Assert.Equal(page, altData.Page);
+            Assert.Equal(displayOrder, altData.DisplayOrder);
+            Assert.Equal(value, altData.Value);
+            Assert.Equal(guid, altData.GUID);
+            Assert.Equal(id, altData.Id);
+            Assert.Equal(deleted, altData.Deleted);
+            Assert.Equal(inactive, altData.Inactive);
+        }
+
+        [Fact]
+        public void MetaData_Constructor_NoParameters()
+        {
+            //arrange, Act
+            AltData altData = new AltData();
+
+            // Assert
+            Assert.Equal(null, altData.SuperClassGUID);
+            Assert.Equal(null, altData.UIConcreteType);
+            Assert.Equal(null, altData.Page);
+            Assert.Equal(null, altData.DisplayOrder);
+            Assert.Equal(null, altData.Value);
+            Assert.Equal(null, altData.GUID);
+            Assert.Equal(0, altData.Id);
+            Assert.Equal(false, altData.Deleted);
+            Assert.Equal(false, altData.Inactive);
+        }
     }
+
 }
