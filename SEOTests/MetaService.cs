7825edeed2778b7567ef952a7ed5a5f08613f390
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-05-01 18:30:41 +0100

    LMWD-17 remove namespace

--- SEOTests/MetaService.cs
+++ SEOTests/MetaService.cs
@@ -2,48 +2,47 @@
 using SEO.Repository.MockMetaRepository;
 using SEO.Service.MetaService;
 
-namespace SEOTests
-{
-    public class MetaServiceTests
-    {
-        [Theory]
-        [InlineData("First", false)]
-        [InlineData("Second", false)]
-        [InlineData("Multiple", false)]
-        [InlineData("Non", false)]
-        [InlineData("Deleted", false)]
-        [InlineData("IncludeInactive", true)]
-        [InlineData("ExcludeInactive", false)]
-        public void GetByPageName_ReturnsMeta(string pageName, bool includeInactive)
-        {
-            // Arrange
-            MockMetaRepository mockMetaRepository = new MockMetaRepository();
-            MetaService metaService = new MetaService(mockMetaRepository);
-            // Act
-            List<MetaData> metaData = metaService.GetByPageName(pageName, includeInactive);
 
-            // Assert
-            if (pageName == "Non" || pageName == "Deleted" || pageName == "ExcludeInactive")
+public class MetaServiceTests
+{
+    [Theory]
+    [InlineData("First", false)]
+    [InlineData("Second", false)]
+    [InlineData("Multiple", false)]
+    [InlineData("Non", false)]
+    [InlineData("Deleted", false)]
+    [InlineData("IncludeInactive", true)]
+    [InlineData("ExcludeInactive", false)]
+    public void GetByPageName_ReturnsMeta(string pageName, bool includeInactive)
+    {
+        // Arrange
+        MockMetaRepository mockMetaRepository = new MockMetaRepository();
+        MetaService metaService = new MetaService(mockMetaRepository);
+        // Act
+        List<MetaData> metaData = metaService.GetByPageName(pageName, includeInactive);
+
+        // Assert
+        if (pageName == "Non" || pageName == "Deleted" || pageName == "ExcludeInactive")
+        {
+            Assert.True(metaData.Count() == 0);
+        }
+        else
+        {
+            foreach (var item in metaData)
             {
-                Assert.True(metaData.Count() == 0); 
+                Assert.Equal(pageName, item.PageName);
             }
+
+            if (pageName == "Multiple")
+            {
+                Assert.True(metaData.Count() == 2);
+            }
+
             else
             {
-                foreach (var item in metaData)
-                {
-                    Assert.Equal(pageName, item.PageName);
-                }
-
-                if (pageName == "Multiple")
-                {
-                    Assert.True(metaData.Count() == 2);
-                }
-
-                else
-                {
-                    Assert.True(metaData.Count() == 1);
-                }
+                Assert.True(metaData.Count() == 1);
             }
         }
     }
 }
+
