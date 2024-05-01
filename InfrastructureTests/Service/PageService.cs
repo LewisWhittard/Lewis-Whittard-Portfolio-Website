8564df393572a81461a46ba88335abc4ca0cefc2
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-05-01 19:01:59 +0100

    LWMD-17 Service namespace

---
+++ InfrastructureTests/Service/PageService.cs
@@ -0,0 +1,48 @@
+﻿using Infrastructure.Service.Page;
+using Infrastructure.Repository.Page;
+
+namespace InfrastructureTests.Service
+{
+    public class PageServiceTests
+    {
+        [Theory]
+        [InlineData("First", false)]
+        [InlineData("Second", false)]
+        [InlineData("Non", false)]
+        [InlineData("Deleted", false)]
+        [InlineData("IncludeInactive", true)]
+        [InlineData("ExcludeInactive", false)]
+        public void GetByPageName_ReturnsPage(string pageName, bool includeInactive)
+        {
+            // Arrange
+            var mockRepository = new MockPageRepository();
+            var pageService = new PageService(mockRepository);
+
+            // Act
+            Infrastructure.Models.Data.Page.Page? page = pageService.GetByPageName(pageName, includeInactive);
+
+            // Assert
+            if (pageName == "Non" || pageName == "Deleted" || pageName == "ExcludeInactive")
+            {
+                Assert.Null(page);
+            }
+            else
+            {
+                Assert.Equal(pageName, page.PageName);
+            }
+        }
+
+        [Fact]
+        [InlineData("HomePage")]
+        public void GetByPageNameAsIDataList_ReturnsIDataList_WhenPageExists()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+            Assert.True(true);
+        }
+    }
+}
