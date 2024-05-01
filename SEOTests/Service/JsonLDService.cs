9b5ca132f33ebe0e02e6dcf75e05e2c4e389095d
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-05-01 18:59:09 +0100

    LMWD-17 SEO Tests Service Namespace

---
+++ SEOTests/Service/JsonLDService.cs
@@ -0,0 +1,49 @@
+﻿using SEO.Models.JsonLD;
+using SEO.Repository.JsonLDRepositoryRepository;
+using SEO.Service.JsonLDService;
+
+namespace SEOTests.Service
+{
+    public class JsonLDServiceTests
+    {
+        [Theory]
+        [InlineData("First", false)]
+        [InlineData("Second", false)]
+        [InlineData("Multiple", false)]
+        [InlineData("Non", false)]
+        [InlineData("Deleted", false)]
+        [InlineData("IncludeInactive", true)]
+        [InlineData("ExcludeInactive", false)]
+        public void GetBySuperClassGUID_ReturnsJsonLD(string superClassGUID, bool includeInactive)
+        {
+            // Arrange
+            MockJsonLDRepository mockJsonLDRepository = new MockJsonLDRepository();
+            JsonLDService jsonLDService = new JsonLDService(mockJsonLDRepository);
+            // Act
+            List<JsonLDData> jsonLDData = jsonLDService.GetBySuperClassGUID(superClassGUID, includeInactive);
+
+            // Assert
+            if (superClassGUID == "Non" || superClassGUID == "Deleted" || superClassGUID == "ExcludeInactive")
+            {
+                Assert.True(jsonLDData.Count() == 0);
+            }
+            else
+            {
+                foreach (var item in jsonLDData)
+                {
+                    Assert.Equal(superClassGUID, item.SuperClassGUID);
+                }
+
+                if (superClassGUID == "Multiple")
+                {
+                    Assert.True(jsonLDData.Count() == 2);
+                }
+
+                else
+                {
+                    Assert.True(jsonLDData.Count() == 1);
+                }
+            }
+        }
+    }
+}
