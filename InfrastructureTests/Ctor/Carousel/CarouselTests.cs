49b96a0277183fbf6ab1ff21c7fd2f74e619bb20
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-05-02 22:59:27 +0100

    LMWD-17 Carousel_Constructor_NoParameters

--- InfrastructureTests/Ctor/Carousel/CarouselTests.cs
+++ InfrastructureTests/Ctor/Carousel/CarouselTests.cs
@@ -1,4 +1,7 @@
-﻿namespace InfrastructureTests.Ctor.Carousel
+﻿using Infrastructure.Models.Data.Interface;
+using Infrastructure.Models.Data.Carousel;
+
+namespace InfrastructureTests.Ctor.Carousel
 {
     public class CarouselCardTests
     {
@@ -6,8 +9,17 @@
         public void Carousel_Constructor_NoParameters()
         {
             //arrange, act
+            Infrastructure.Models.Data.Carousel.Carousel carousel = new Infrastructure.Models.Data.Carousel.Carousel();
 
             //assert
+            Assert.Equal(0, carousel.Id);
+            Assert.False(carousel.Deleted);
+            Assert.False(carousel.Inactive);
+            Assert.Null(carousel.Images);
+            Assert.Null(carousel.DisplayOrder);
+            Assert.Equal(UIConcrete.Carousel, carousel.UIConcreteType);
+            Assert.Null(carousel.GUID);
+            Assert.Equal("", carousel.GUID);
         }
     }
 }
