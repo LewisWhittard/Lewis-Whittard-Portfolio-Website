a30606a6151f75f2b85785919f0469f2ba1068d9
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-05-18 18:27:47 +0100

    LMWD-17 Table_Constructor_NoParameters

--- InfrastructureTests/Ctor/Table/TableTests.cs
+++ InfrastructureTests/Ctor/Table/TableTests.cs
@@ -1,14 +1,26 @@
-﻿namespace InfrastructureTests.Ctor
+﻿using Infrastructure.Models.Data.Interface;
+using Infrastructure.Models.Data.Table;
+
+namespace InfrastructureTests.Ctor
 {
     public class TableTests
     {
         [Fact]
         public void Table_Constructor_NoParameters()
         {
-            //arrange, act
-            Assert.True(false);
+            // Arrange & Act
+            var table = new Table();
 
-            //assert
+            // Assert
+            Assert.Equal(0, table.Id);
+            Assert.Null(table.Title);
+            Assert.False(table.Deleted);
+            Assert.False(table.Inactive);
+            Assert.Null(table.Headers);
+            Assert.Null(table.Columns);
+            Assert.Null(table.DisplayOrder);
+            Assert.Null(table.GUID);
+            Assert.Null(table.UIConcreteType);
         }
     }
 }
