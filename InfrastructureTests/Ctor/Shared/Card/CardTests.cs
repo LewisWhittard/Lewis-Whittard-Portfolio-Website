53286d458aad356f42938501967ceaf96f404712
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-05-18 18:49:26 +0100

    LMWD-17 Card_Constructor_NoParameters() and Image_Constructor_NoParameters()

--- InfrastructureTests/Ctor/Shared/Card/CardTests.cs
+++ InfrastructureTests/Ctor/Shared/Card/CardTests.cs
@@ -1,14 +1,27 @@
-﻿namespace InfrastructureTests.Ctor
+﻿using Infrastructure.Models.Data.Interface;
+using Infrastructure.Models.Data.Shared.Card;
+
+namespace InfrastructureTests.Ctor
 {
     public class CardTests
     {
         [Fact]
         public void Card_Constructor_NoParameters()
         {
-            //arrange, act
-            Assert.True(false);
+            // Arrange & Act
+            Card card = new Card();
 
-            //assert
+            // Assert
+            Assert.Equal(UIConcrete.Card, card.UIConcreteType);
+            Assert.Null(card.Image);
+            Assert.Null(card.Title);
+            Assert.Null(card.Description);
+            Assert.Null(card.Navigation);
+            Assert.Equal(0, card.Id);
+            Assert.False(card.Deleted);
+            Assert.False(card.Inactive);
+            Assert.Null(card.DisplayOrder);
+            Assert.Null(card.GUID);
         }
     }
 }
--- InfrastructureTests/Ctor/Shared/Image/ImageTests.cs
+++ InfrastructureTests/Ctor/Shared/Image/ImageTests.cs
@@ -1,14 +1,24 @@
-﻿namespace InfrastructureTests.Ctor
+﻿using Infrastructure.Models.Data.Interface;
+using Infrastructure.Models.Data.Shared.Image;
+
+namespace InfrastructureTests.Ctor
 {
     public class ImageTests
     {
         [Fact]
         public void Image_Constructor_NoParameters()
         {
-            //arrange, act
-            Assert.True(false);
+            // Arrange & Act
+            var image = new Image();
 
-            //assert
+            // Assert
+            Assert.Null(image.Source);
+            Assert.Null(image.DisplayOrder);
+            Assert.Equal(0, image.Id);
+            Assert.False(image.Deleted);
+            Assert.False(image.Inactive);
+            Assert.Null(image.GUID);
+            Assert.Equal(UIConcrete.Image, image.UIConcreteType);
         }
     }
 }
