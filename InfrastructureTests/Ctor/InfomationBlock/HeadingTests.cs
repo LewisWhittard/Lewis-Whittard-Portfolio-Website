1d2045f4896868ffd6d7233570a8552df6e83ee7
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-05-19 12:10:31 +0100

    Header_Constructor_NoParameters()

--- InfrastructureTests/Ctor/InfomationBlock/HeadingTests.cs
+++ InfrastructureTests/Ctor/InfomationBlock/HeadingTests.cs
@@ -1,11 +1,27 @@
-﻿namespace InfrastructureTests.Ctor
+﻿using Infrastructure.Models.Data.InfomationBlock;
+using Infrastructure.Models.Data.Interface;
+
+namespace InfrastructureTests.Ctor
 {
     public class HeadingTests
     {
         [Fact]
-        public void Heading_Constructor_NoParameters()
+        public void Header_Constructor_NoParameters()
         {
-            Assert.Fail();
+            // Arrange & Act
+            var heading = new Heading();
+
+            // Assert
+            Assert.NotNull(heading);
+            Assert.Equal(0, heading.Id);
+            Assert.False(heading.Deleted);
+            Assert.False(heading.Inactive);
+            Assert.Null(heading.Text);
+            Assert.Null(heading.DisplayOrder);
+            Assert.Equal(0, heading.InfomationBlockid);
+            Assert.Null(heading.GUID);
+            Assert.Equal(0, heading.Level);
+            Assert.Equal(UIConcrete.Heading, heading.UIConcreteType);
         }
     }
 }
