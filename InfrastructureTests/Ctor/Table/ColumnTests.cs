4bf53f0e7df9dca82020ef3b6c518c05f0709aba
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-05-19 12:45:34 +0100

    LMWD-17  Header_Constructor_NoParameters() and Column_Constructor_NoParameters()

--- InfrastructureTests/Ctor/Table/ColumnTests.cs
+++ InfrastructureTests/Ctor/Table/ColumnTests.cs
@@ -1,11 +1,26 @@
-﻿namespace InfrastructureTests.Ctor
+﻿using Infrastructure.Models.Data.Interface;
+using Infrastructure.Models.Data.Table;
+
+namespace InfrastructureTests.Ctor
 {
     public class ColumnTests
     {
         [Fact]
         public void Column_Constructor_NoParameters()
         {
-            Assert.Fail();
+            // Act and arrange
+            var column = new Column();
+
+
+            // Assert
+            Assert.Equal(0, column.Id);
+            Assert.False(column.Deleted);
+            Assert.False(column.Inactive);
+            Assert.Null(column.Value);
+            Assert.Null(column.DisplayOrder);
+            Assert.Equal(0, column.TableID);
+            Assert.Null(column.GUID);
+            Assert.Equal(UIConcrete.Column, column.UIConcreteType);
         }
     }
 }
--- InfrastructureTests/Ctor/Table/HeaderTests.cs
+++ InfrastructureTests/Ctor/Table/HeaderTests.cs
@@ -1,11 +1,25 @@
-﻿namespace InfrastructureTests.Ctor
+﻿using Infrastructure.Models.Data.Interface;
+using Infrastructure.Models.Data.Table;
+
+namespace InfrastructureTests.Ctor
 {
     public class HeaderTests
     {
         [Fact]
         public void Header_Constructor_NoParameters()
         {
-            Assert.Fail();
+            // Arrange and Act
+            var header = new Header();
+
+            // Assert
+            Assert.Equal(0, header.Id);
+            Assert.False(header.Deleted);
+            Assert.False(header.Inactive);
+            Assert.Null(header.DisplayOrder);
+            Assert.Equal(0, header.TableID);
+            Assert.Null(header.Value);
+            Assert.Null(header.GUID);
+            Assert.Equal(UIConcrete.Header, header.UIConcreteType);
         }
     }
 }
