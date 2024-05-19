b92bd2abed31970472eaa6b04920a4fceb280dbf
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-05-19 11:30:49 +0100

    Paragraph_Constructor_NoParameters()

--- InfrastructureTests/Ctor/InfomationBlock/Paragraphtests.cs
+++ InfrastructureTests/Ctor/InfomationBlock/Paragraphtests.cs
@@ -1,11 +1,25 @@
-﻿namespace InfrastructureTests.Ctor
+﻿using Infrastructure.Models.Data.InfomationBlock;
+using Infrastructure.Models.Data.Interface;
+
+namespace InfrastructureTests.Ctor
 {
     public class ParagraphTests
     {
         [Fact]
         public void Paragraph_Constructor_NoParameters()
         {
-            Assert.Fail();
+            // Arrange & Act
+            var paragraph = new Paragraph();
+
+            // Assert
+            Assert.Equal(UIConcrete.Paragraph, paragraph.UIConcreteType);
+            Assert.Null(paragraph.Text);
+            Assert.Null(paragraph.DisplayOrder);
+            Assert.Equal(0, paragraph.Id);
+            Assert.False(paragraph.Deleted);
+            Assert.False(paragraph.Inactive);
+            Assert.Equal(0, paragraph.InfomationBlockid);
+            Assert.Null(paragraph.GUID);
         }
     }
 }
