0c7cc1cf432c62616de47e7ce07a5ac936e825c3
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-05-23 23:21:02 +0100

    LMWD-17 Constructor_InitializesPropertiesCorrectly

--- InfrastructureTests/Ctor/Shared/Image/ImageTests.cs
+++ InfrastructureTests/Ctor/Shared/Image/ImageTests.cs
@@ -20,5 +20,22 @@ namespace InfrastructureTests.Ctor
             Assert.Null(image.GUID);
             Assert.Equal(UIConcrete.Image, image.UIConcreteType);
         }
+
+        [Theory]
+        [InlineData("example.jpg", 10, 1, false, true, "ABC123")]
+        [InlineData("anotherexample.png", null, 2, true, false, "XYZ789")]
+        public void Constructor_InitializesPropertiesCorrectly(string source, int? displayOrder, int id, bool deleted, bool inactive, string gUID)
+        {
+            // Act
+            var image = new Image(source, displayOrder, id, deleted, inactive, gUID);
+
+            // Assert
+            Assert.Equal(source, image.Source);
+            Assert.Equal(displayOrder, image.DisplayOrder);
+            Assert.Equal(id, image.Id);
+            Assert.Equal(deleted, image.Deleted);
+            Assert.Equal(inactive, image.Inactive);
+            Assert.Equal(gUID, image.GUID);
+        }
     }
 }
