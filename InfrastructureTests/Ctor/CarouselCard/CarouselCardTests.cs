f2c02334496fec435e255d6d77040bc8c0659f51
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-05-29 23:26:56 +0100

    LWMD-17 ParameterizedConstructor_ShouldInitializeWithGivenValues

--- InfrastructureTests/Ctor/CarouselCard/CarouselCardTests.cs
+++ InfrastructureTests/Ctor/CarouselCard/CarouselCardTests.cs
@@ -1,9 +1,18 @@
 ﻿using Infrastructure.Models.Data.Interface;
+using Infrastructure.Models.Data.Shared.Card;
+using Infrastructure.Models.Data.Shared.Image;
 
 namespace InfrastructureTests.Ctor
 {
     public class CarouselCardTests
     {
+        private List<Card> _cards;
+
+        public void SetUp()
+        {
+            Image _image = new Image();
+        }
+
         [Fact]
         public void CarouselCard_Constructor_NoParameters()
         {
@@ -19,6 +28,36 @@ namespace InfrastructureTests.Ctor
             Assert.Null(carouselCard.DisplayOrder);
             Assert.Equal(UIConcrete.CarouselCard, carouselCard.UIConcreteType);
             Assert.Null(carouselCard.GUID);
+        }
+
+        [Theory]
+        [InlineData(1, true, false, 5, "test-guid")]
+        [InlineData(2, false, true, 10, "another-guid")]
+        [InlineData(1, true, true, 5, "test-guid")]
+        [InlineData(2, false, false, 10, "another-guid")]
+        public void ParameterizedConstructor_ShouldInitializeWithGivenValues(
+            int id, bool deleted, bool inactive, int? displayOrder, string gUID)
+        {
+            SetUp();
+
+            // Arrange,Act
+            var carouselCard = new Infrastructure.Models.Data.CarouselCard.CarouselCard(id, deleted, inactive, _cards, displayOrder, gUID);
+
+            // Assert
+            Assert.Equal(id, carouselCard.Id);
+            Assert.Equal(deleted, carouselCard.Deleted);
+            Assert.Equal(inactive, carouselCard.Inactive);
+            Assert.Equal(_cards, carouselCard.Cards);
+            Assert.Equal(displayOrder, carouselCard.DisplayOrder);
+            Assert.Equal(gUID, carouselCard.GUID);
+            Assert.Equal(UIConcrete.CarouselCard, carouselCard.UIConcreteType);
+
+            TearDown();
+        }
+
+        public void TearDown()
+        {
+
         }
     }
 }
