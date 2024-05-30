2e7c49ae0ae7aec9eabd8c25fb2b42bb0c950b51
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-05-30 21:38:15 +0100

    LMWD-17 Constructor_Parameterized_ShouldInitializeProperties

--- InfrastructureTests/Ctor/Carousel/CarouselTests.cs
+++ InfrastructureTests/Ctor/Carousel/CarouselTests.cs
@@ -1,14 +1,28 @@
-﻿using Infrastructure.Models.Data.Interface;
+﻿using Infrastructure.Models.Data.Carousel;
+using Infrastructure.Models.Data.Interface;
+using Infrastructure.Models.Data.Shared.Image;
 
 namespace InfrastructureTests.Ctor
 {
     public class CarouselTests
     {
+        public List<Image> _images { get; set; }
+
+
+
+        public void SetUp()
+        {
+            _images = new List<Image>();
+
+            _images.Add(new Image("ImageSource0", 0, 0, false, false, "ImageGuid0"));
+            _images.Add(new Image("ImageSource1", 1, 1, false, false, "ImageGuid1"));
+            _images.Add(new Image("ImageSource2", 2, 2, false, false, "ImageGuid2"));
+        }
         [Fact]
         public void Carousel_Constructor_NoParameters()
         {
             //arrange, act
-            Infrastructure.Models.Data.Carousel.Carousel carousel = new Infrastructure.Models.Data.Carousel.Carousel();
+            Carousel carousel = new Carousel();
 
             //assert
             Assert.Equal(0, carousel.Id);
@@ -18,6 +32,32 @@ namespace InfrastructureTests.Ctor
             Assert.Null(carousel.DisplayOrder);
             Assert.Equal(UIConcrete.Carousel, carousel.UIConcreteType);
             Assert.Null(carousel.GUID);
+        }
+
+        [Theory]
+        [InlineData(1, true, false, 4, "guid1")]
+        [InlineData(2, false, true, 3, "guid2")]
+        [InlineData(3, false, false, 2, "guid3")]
+        [InlineData(4, true, true, 1, "guid4")]
+        public void Constructor_Parameterized_ShouldInitializeProperties(int id, bool deleted, bool inactive, int displayOrder, string guid)
+        {
+            // Act, arrange
+            SetUp();
+            var carousel = new Carousel(id, deleted, inactive, _images, displayOrder, guid);
+
+            // Assert
+            Assert.Equal(id, carousel.Id);
+            Assert.Equal(deleted, carousel.Deleted);
+            Assert.Equal(inactive, carousel.Inactive);
+            Assert.Equal(_images, carousel.Images);
+            Assert.Equal(displayOrder, carousel.DisplayOrder);
+            Assert.Equal(UIConcrete.Carousel, carousel.UIConcreteType);
+            Assert.Equal(guid, carousel.GUID);
+        }
+
+        public void TearDown()
+        {
+            _images.Clear();
         }
     }
 }
