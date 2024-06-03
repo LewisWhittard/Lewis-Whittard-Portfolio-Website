7ba9fbfe367735945fee512deb8133f5b3660379
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-06-03 22:58:35 +0100

    LWMD-17 undo mock page changes

--- Infrastructure/Repository/Page/MockPageRepository.cs
+++ Infrastructure/Repository/Page/MockPageRepository.cs
@@ -15,41 +15,41 @@ namespace Infrastructure.Repository.Page
 
         public MockPageRepository()
         {
-            //// Create lists to hold the instances
-            //List<Card> cards = new List<Card>();
-            //List<Carousel> carousels = new List<Carousel>();
-            //List<CarouselCard> carouselCards = new List<CarouselCard>();
-            //List<Table> tables = new List<Table>();
-            //List<InfomatonBlock> infoBlocks = new List<InfomatonBlock>();
-            //List<Video> videos = new List<Video>();
+            // Create lists to hold the instances
+            List<Card> cards = new List<Card>();
+            List<Carousel> carousels = new List<Carousel>();
+            List<CarouselCard> carouselCards = new List<CarouselCard>();
+            List<Table> tables = new List<Table>();
+            List<InfomatonBlock> infoBlocks = new List<InfomatonBlock>();
+            List<Video> videos = new List<Video>();
 
-            //// Add instances to the lists
-            //cards.Add((new Card(null,"","","",0,false,false,0,"",0)));
-            ////cards.Add(new Card() { Id = 0, Deleted = false, Inactive = false});
-            //carousels.Add(new Carousel() { Id = 0, Deleted = false, Inactive = false });
-            //carouselCards.Add(new CarouselCard() { Id = 0, Deleted = false, Inactive = false });
-            //tables.Add(new Table() { Id = 0, Deleted = false, Inactive = false });
-            //infoBlocks.Add(new InfomatonBlock() { Id = 0, Deleted = false, Inactive = false });
-            //videos.Add(new Video() { Id = 0, Deleted = false, Inactive = false });
+            // Add instances to the lists
+            cards.Add((new Card(null, "", "", "", 0, false, false, 0, "", 0)));
+            //cards.Add(new Card() { Id = 0, Deleted = false, Inactive = false});
+            carousels.Add(new Carousel() { Id = 0, Deleted = false, Inactive = false });
+            carouselCards.Add(new CarouselCard() { Id = 0, Deleted = false, Inactive = false });
+            tables.Add(new Table() { Id = 0, Deleted = false, Inactive = false });
+            infoBlocks.Add(new InfomatonBlock() { Id = 0, Deleted = false, Inactive = false });
+            videos.Add(new Video() { Id = 0, Deleted = false, Inactive = false });
 
-            //cards.Add(new Card() { Id = 1, Deleted = false, Inactive = false });
-            //carousels.Add(new Carousel() { Id = 1, Deleted = false, Inactive = false });
-            //carouselCards.Add(new CarouselCard() { Id = 1, Deleted = false, Inactive = false });
-            //tables.Add(new Table() { Id = 1, Deleted = false, Inactive = false });
-            //infoBlocks.Add(new InfomatonBlock() { Id = 1, Deleted = false, Inactive = false });
-            //videos.Add(new Video() { Id = 1, Deleted = false, Inactive = false });
+            cards.Add(new Card() { Id = 1, Deleted = false, Inactive = false });
+            carousels.Add(new Carousel() { Id = 1, Deleted = false, Inactive = false });
+            carouselCards.Add(new CarouselCard() { Id = 1, Deleted = false, Inactive = false });
+            tables.Add(new Table() { Id = 1, Deleted = false, Inactive = false });
+            infoBlocks.Add(new InfomatonBlock() { Id = 1, Deleted = false, Inactive = false });
+            videos.Add(new Video() { Id = 1, Deleted = false, Inactive = false });
 
-            //_pages = new List<Models.Data.Page.Page>()
-            //{
-            //    new Models.Data.Page.Page() {PageName = "First", Deleted = false, Inactive = false, Cards = cards, Carousels = carousels, CarouselCards = carouselCards, Tables = tables, Videos = videos, InformationBlocks = infoBlocks},
-            //    new Models.Data.Page.Page() {PageName = "Second", Deleted = false, Inactive = false},
-            //    new Models.Data.Page.Page() {PageName = "Deleted", Deleted = true, Inactive = false},
-            //    new Models.Data.Page.Page() {PageName = "IncludeInactive", Deleted = false, Inactive = true},
-            //    new Models.Data.Page.Page() {PageName = "ExcludeInactive", Deleted = false, Inactive = true}
-            //};
-            
+            _pages = new List<Models.Data.Page.Page>()
+            {
+                new Models.Data.Page.Page() {PageName = "First", Deleted = false, Inactive = false, Cards = cards, Carousels = carousels, CarouselCards = carouselCards, Tables = tables, Videos = videos, InformationBlocks = infoBlocks},
+                new Models.Data.Page.Page() {PageName = "Second", Deleted = false, Inactive = false},
+                new Models.Data.Page.Page() {PageName = "Deleted", Deleted = true, Inactive = false},
+                new Models.Data.Page.Page() {PageName = "IncludeInactive", Deleted = false, Inactive = true},
+                new Models.Data.Page.Page() {PageName = "ExcludeInactive", Deleted = false, Inactive = true}
+            };
 
-            
+
+
 
         }
 
