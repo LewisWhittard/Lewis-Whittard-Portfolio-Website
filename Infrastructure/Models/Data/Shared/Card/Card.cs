457c292b20c504be908329a72c6fd7e0fba79987
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-06-03 23:17:26 +0100

    LWMD-17 Card

--- Infrastructure/Models/Data/Shared/Card/Card.cs
+++ Infrastructure/Models/Data/Shared/Card/Card.cs
@@ -6,20 +6,20 @@ namespace Infrastructure.Models.Data.Shared.Card
 {
     public class Card : ICard, IData
     {
-        public Image.Image Image { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string Navigation { get; set; }
-        public int Id { get; set; }
-        public bool Deleted { get; set; }
-        public bool Inactive { get; set; }
-        public int? DisplayOrder { get; set; }
-        public string GUID { get; set; }
-        public int? PageId { get; set; }
+        public Image.Image Image { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public string Navigation { get; private set; }
+        public int Id { get; private set; }
+        public bool Deleted { get; private set; }
+        public bool Inactive { get; private set; }
+        public int? DisplayOrder { get; private set; }
+        public string GUID { get; private set; }
+        public int? PageId { get; private set; }
         [JsonIgnore]
-        public UIConcrete? UIConcreteType { get; set; }
+        public UIConcrete? UIConcreteType { get; private set; }
 
-        public Card(Image.Image image, string title, string description, string navigation,int id,bool deleted,bool inactive,int displayOrder,string gUID, int? pageId)
+        public Card(Image.Image image, string title, string description, string navigation, int id, bool deleted, bool inactive, int displayOrder, string gUID, int? pageId)
         {
             Image = image;
             Title = title;
