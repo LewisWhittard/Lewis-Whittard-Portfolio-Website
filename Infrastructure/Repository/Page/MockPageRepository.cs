37c08cb84cebe010fe559d62f0302f7d99a0f269
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-06-05 22:10:59 +0100

    LMWD-17 Page service tests fix

--- Infrastructure/Repository/Page/MockPageRepository.cs
+++ Infrastructure/Repository/Page/MockPageRepository.cs
@@ -2,7 +2,6 @@
 using Infrastructure.Models.Data.CarouselCard;
 using Infrastructure.Models.Data.InformationBlock;
 using Infrastructure.Models.Data.Shared.Card;
-using Infrastructure.Models.Data.Shared.Image;
 using Infrastructure.Models.Data.Table;
 using Infrastructure.Models.Data.Video;
 using Infrastructure.Repository.Page.Interface;
@@ -15,9 +14,44 @@ namespace Infrastructure.Repository.Page
 
         public MockPageRepository()
         {
-            _pages = new List<Models.Data.Page.Page>();
+            List<Card> cards = new List<Card>()
+            {
+                new Card(),
+                new Card()
+            };
 
-            _pages.Add(new Models.Data.Page.Page("First", null, null, null, null, null, null, "FirstGUID", 0, false, false));
+            List<Carousel> carousels = new List<Carousel>()
+            {
+                new Carousel(),
+                new Carousel()
+            };
+
+            List<CarouselCard> carouselCards = new List<CarouselCard>()
+            {
+                new CarouselCard(),
+                new CarouselCard()
+            };
+
+            List<InfomatonBlock> informationBlocks = new List<InfomatonBlock>()
+            {
+                new InfomatonBlock(),
+                new InfomatonBlock()
+            };
+
+            List<Table> tables = new List<Table>()
+            {
+                new Table(),
+                new Table()
+            };
+
+            List<Video> videos = new List<Video>()
+            {
+                new Video(),
+                new Video()
+            };
+
+            _pages = new List<Models.Data.Page.Page>();
+            _pages.Add(new Models.Data.Page.Page("First", cards, carousels, carouselCards, informationBlocks, tables, videos, "FirstGUID", 0, false, false));
             _pages.Add(new Models.Data.Page.Page("Second", null, null, null, null, null, null, "SecondGUID", 0, false, false));
             _pages.Add(new Models.Data.Page.Page("Deleted", null, null, null, null, null, null, "DeletedGUID", 0, true, false));
             _pages.Add(new Models.Data.Page.Page("IncludeInactive", null, null, null, null, null, null, "IncludeInactiveGUID", 0, false, true));
