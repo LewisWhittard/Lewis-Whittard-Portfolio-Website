88f55af81c00fe2428d5b49dfbbe1ca6d1f14489
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-06-06 22:23:38 +0100

    LWMD-17 Head

---
+++ Infrastructure/Models/Data/Head/Head.cs
@@ -0,0 +1,32 @@
+﻿using Infrastructure.Models.Data.Head.Interface;
+using Infrastructure.Models.Data.Interface;
+
+namespace Infrastructure.Models.Data.Head
+{
+    public class Head : IData, IHead
+    {
+        public int Id { get; private set; }
+        public int? DisplayOrder { get; private set; }
+        public string? GUID { get; private set; }
+        public bool Deleted { get; private set; }
+        public bool Inactive { get; private set; }
+        public UIConcrete? UIConcreteType { get; private set; }
+        public string Title { get; private set; }
+
+        public Head(int id, int? displayOrder, bool deleted, bool inactive, string title)
+        {
+            Id = id;
+            DisplayOrder = displayOrder;
+            GUID = null;
+            Deleted = deleted;
+            Inactive = inactive;
+            UIConcreteType = UIConcrete.Head;
+            Title = title;
+        }
+
+        public Head()
+        {
+            UIConcreteType = UIConcrete.Head;
+        }
+    }
+}
---
+++ Infrastructure/Models/Data/Head/Interface/Head.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.Data.Head.Interface
+{
+    public interface IHead
+    {
+        string Title { get; }
+    }
+}
