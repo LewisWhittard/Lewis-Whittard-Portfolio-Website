553e23e5053ca13ec1234687c0459547e7696521
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-06-06 22:40:24 +0100

    LWMD-17 remove unused statements

--- Infrastructure/Models/Data/Head/Interface/Head.cs
+++ Infrastructure/Models/Data/Head/Interface/Head.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Infrastructure.Models.Data.Head.Interface
+﻿namespace Infrastructure.Models.Data.Head.Interface
 {
     public interface IHead
     {
