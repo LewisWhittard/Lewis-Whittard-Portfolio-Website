87df4da8a5cc006f66324ebe5db85bb15d8c0417
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-06-07 21:04:54 +0100

    LWMD-17 Image Concrete

--- UIFactory/Factory/Concrete/Shared/Card/Image/Image.cs
+++ UIFactory/Factory/Concrete/Shared/Card/Image/Image.cs
@@ -1,12 +1,15 @@
-﻿using UIFactory.Factory.Concrete.Shared.Card.Image.Interface;
+﻿using Infrastructure.Models.Data.Interface;
+using UIFactory.Factory.Concrete.Interface;
+using UIFactory.Factory.Concrete.Shared.Card.Image.Interface;
 
 namespace UIFactory.Factory.Concrete.Shared.Card.Image
 {
-    public class Image : IImage
+    public class Image : IImage, IConcreteUI
     {
-        public Infrastructure.Models.Data.Shared.Image.Image ImageData { get; private set; } 
+        public Infrastructure.Models.Data.Shared.Image.Image ImageData { get; private set; }
         public SEO.Models.Alt.AltData? AltData { get; private set; }
         public List<SEO.Models.JsonLD.JsonLDData>? TitleData { get; private set; }
+        public UIConcrete UIConcrete { get; private set; }
 
         private readonly Infrastructure.Models.Data.Shared.Image.Image _imageData;
         private readonly SEO.Service.AltService.AltService? _altService;
@@ -16,7 +19,8 @@ namespace UIFactory.Factory.Concrete.Shared.Card.Image
         public Image(Infrastructure.Models.Data.Shared.Image.Image imageData, SEO.Service.AltService.AltService? altService, SEO.Service.JsonLDService.JsonLDService? jsonLDService)
         {
             _imageData = imageData;
-            ImageData = imageData;
+            ImageData = _imageData;
+            UIConcrete = UIConcrete.Image;
             if (altService != null)
             {
                 _altService = altService;
@@ -28,10 +32,10 @@ namespace UIFactory.Factory.Concrete.Shared.Card.Image
                 SetJsonLDData();
             }
         }
-        
+
         private void SetAltData()
         {
-            AltData = _altService.GetBySuperClassGUID(ImageData.GUID,false);
+            AltData = _altService.GetBySuperClassGUID(ImageData.GUID, false);
         }
 
 
