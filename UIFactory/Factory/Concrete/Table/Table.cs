14730155593c8faf45c0f8c2299a4b51ed88dc8c
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-06-08 23:28:24 +0100

    LWMD-17 remove comments

--- UIFactory/Factory/Concrete/Table/Table.cs
+++ UIFactory/Factory/Concrete/Table/Table.cs
@@ -9,11 +9,11 @@ namespace UIFactory.Factory.Concrete.Table
         public Infrastructure.Models.Data.Table.Table TableData { get; private set; }
         public List<JsonLDData> JsonLDs { get; private set; }
 
-        //private read only JsonLDservice and Table Data
+
         private readonly SEO.Service.JsonLDService.JsonLDService _jsonLDService;
         private readonly Infrastructure.Models.Data.Table.Table _table;
 
-        //ctor with JsonLDService and Table Data
+
         public Table(SEO.Service.JsonLDService.JsonLDService jsonLDService, Infrastructure.Models.Data.Table.Table table)
         {
             _jsonLDService = jsonLDService;
