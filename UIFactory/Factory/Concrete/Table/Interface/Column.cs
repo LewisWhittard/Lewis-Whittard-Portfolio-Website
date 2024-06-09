649f33bd9721d400eb67cd63231493c596287340
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-06-09 12:01:48 +0100

    LWMD-17 Column and header interface

---
+++ UIFactory/Factory/Concrete/Table/Interface/Column.cs
@@ -0,0 +1,7 @@
+﻿namespace UIFactory.Factory.Concrete.Table.Interface
+{
+    public interface IColumn
+    {
+        public Infrastructure.Models.Data.Table.Column ColumnData { get; }
+    }
+}
---
+++ UIFactory/Factory/Concrete/Table/Interface/Header.cs
@@ -0,0 +1,7 @@
+﻿namespace UIFactory.Factory.Concrete.Table.Interface
+{
+    public interface IHeader
+    {
+        public Infrastructure.Models.Data.Table.Header HeaderData { get; }
+    }
+}
