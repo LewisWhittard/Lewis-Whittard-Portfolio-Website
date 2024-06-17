d874cf250ba1ba5525d42c110067d72adcad636e
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-06-17 08:52:09 +0100

    LWMD-17 Carousel Interface

---
+++ UIFactory/Factory/Concrete/Carousel/Interface/Carousel.cs
@@ -0,0 +1,9 @@
+﻿namespace UIFactory.Factory.Concrete.Carousel.Interface
+{
+    public interface ICarousel
+    {
+        Infrastructure.Models.Data.Carousel.Carousel CarouselData { get; }
+        List<SEO.Models.JsonLD.JsonLDData>? JsonLDDatas { get; }
+        List<Shared.Image.Image> Images { get; }
+    }
+}
