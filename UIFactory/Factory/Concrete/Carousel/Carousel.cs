4fe354a85ba113962e27b80e394b7db60199fec3
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-06-17 08:25:01 +0100

    LWWD-17 Video concrete

---
+++ UIFactory/Factory/Concrete/Carousel/Carousel.cs
@@ -0,0 +1,12 @@
+﻿namespace UIFactory.Factory.Concrete.Carousel
+{
+    public class Carousel
+    {
+        public Infrastructure.Models.Data.Carousel.Carousel CarouselData { get; private set; }
+        public SEO.Models.JsonLD.JsonLDData JsonLDData { get; private set; }
+
+        private readonly Infrastructure.Models.Data.Carousel.Carousel _carouselData;
+        private readonly SEO.Services.JsonLDService _jsonLDService;
+
+    }
+}
