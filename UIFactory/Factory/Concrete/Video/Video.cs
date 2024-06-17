f6fd9bb0beb7c5e6fc4736f70e474c91e9fce2fa
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-06-17 08:25:25 +0100

    LMWD-17 Video Concrete

--- UIFactory/Factory/Concrete/Video/Video.cs
+++ UIFactory/Factory/Concrete/Video/Video.cs
@@ -6,18 +6,30 @@ namespace UIFactory.Factory.Concrete.Video
 {
     public class Video : IVideo
     {
-        public Infrastructure.Models.Data.Video.Video VideoData { get; }
-        public List<JsonLDData>? JsonLDDatas { get; }
+        public Infrastructure.Models.Data.Video.Video VideoData { get; private set; }
+        public List<JsonLDData>? JsonLDDatas { get; private set; }
 
         private readonly Infrastructure.Models.Data.Video.Video _video;
         private readonly JsonLDService? _jsonLDService;
 
-        public Video(Infrastructure.Models.Data.Video.Video videoData, JsonLDService jsonLDService)
+        public Video(Infrastructure.Models.Data.Video.Video videoData, JsonLDService? jsonLDService)
         {
             _video = videoData;
             _jsonLDService = jsonLDService;
             VideoData = _video;
-            JsonLDDatas = _jsonLDService.GetBySuperClassGUID(_video.GUID,false);
+            SetJsonLD();
+        }
+
+        public void SetJsonLD()
+        {
+            if (_jsonLDService != null)
+            {
+                JsonLDDatas = _jsonLDService.GetBySuperClassGUID(_video.GUID, false);
+            }
+            else
+            {
+                JsonLDDatas = null;
+            }
         }
     }
 }
