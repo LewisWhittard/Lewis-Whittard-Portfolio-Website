21f18b1b04c2dc8db2e3e13294fe0756ecaddcf2
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-06-17 21:37:36 +0100

    LMWD-17 Infomation Block

--- UIFactory/Factory/Concrete/InformationBlock/InformationBlock.cs
+++ UIFactory/Factory/Concrete/InformationBlock/InformationBlock.cs
@@ -1,27 +1,32 @@
-﻿using Infrastructure.Models.Data.Shared.Image;
+﻿using SEO.Service.AltService;
+using UIFactory.Factory.Concrete.InformationBlock.Interface;
+using UIFactory.Factory.Concrete.Shared.Image;
 
 namespace UIFactory.Factory.Concrete.InformationBlock
 {
-    public class InformationBlock
+    public class InformationBlock : IInformationBlock
     {
         public Infrastructure.Models.Data.InformationBlock.InfomatonBlock InformationBlockData { get; private set; }
         public List<SEO.Models.JsonLD.JsonLDData>? JsonLDData { get; private set; }
         public List<Heading>? Headings { get; private set; }
         public List<Paragraph>? Paragraphs { get; private set; }
-        public List<Image> images { get; private set; }
+        public List<Image>? Images { get; private set; }
 
         private readonly Infrastructure.Models.Data.InformationBlock.InfomatonBlock _informationBlockDatas;
         private readonly SEO.Service.JsonLDService.JsonLDService _jsonLDService;
+        private readonly AltService _altService;
 
 
-        public InformationBlock(Infrastructure.Models.Data.InformationBlock.InfomatonBlock informationBlockDatas, SEO.Service.JsonLDService.JsonLDService jsonLDService)
+        public InformationBlock(Infrastructure.Models.Data.InformationBlock.InfomatonBlock informationBlockDatas, SEO.Service.JsonLDService.JsonLDService jsonLDService, AltService altService)
         {
             _informationBlockDatas = informationBlockDatas;
             InformationBlockData = _informationBlockDatas;
             _jsonLDService = jsonLDService;
+            _altService = altService;
             SetJsonLD();
             SetHeadings();
             SetParagraphs();
+            SetImages();
         }
 
         private void SetJsonLD()
@@ -65,6 +70,22 @@ namespace UIFactory.Factory.Concrete.InformationBlock
             else
             {
                 Paragraphs = null;
+            }
+        }
+
+        public void SetImages()
+        {
+            if (InformationBlockData != null)
+            {
+                Images = new List<Image>();
+                foreach (var image in InformationBlockData.Images)
+                {
+                    Images.Add(new Image(image, _altService,_jsonLDService));
+                }
+            }
+            else
+            {
+                Images = null;
             }
         }
     }
---
+++ UIFactory/Factory/Concrete/InformationBlock/Interface/InformationBlock.cs
@@ -0,0 +1,12 @@
+﻿using UIFactory.Factory.Concrete.Shared.Image;
+namespace UIFactory.Factory.Concrete.InformationBlock.Interface
+{
+    public interface IInformationBlock
+    {
+        Infrastructure.Models.Data.InformationBlock.InfomatonBlock InformationBlockData { get; }
+        List<SEO.Models.JsonLD.JsonLDData>? JsonLDData { get; }
+        List<Heading>? Headings { get; }
+        List<Paragraph>? Paragraphs { get; }
+        List<Image>? Images { get; }
+    }
+}
