db164b6853f8cf86343d281a4b997124efaa67e8
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-06-17 20:20:31 +0100

    LWMD-17 Paragraph and Heading

---
+++ UIFactory/Factory/Concrete/InformationBlock/Heading.cs
@@ -0,0 +1,15 @@
+﻿namespace UIFactory.Factory.Concrete.InformationBlock
+{
+    public class Heading
+    {
+        public Infrastructure.Models.Data.InformationBlock.Heading HeadingData { get; private set; }
+
+        private readonly Infrastructure.Models.Data.InformationBlock.Heading _heading;
+
+        public Heading(Infrastructure.Models.Data.InformationBlock.Heading heading)
+        {
+            _heading = heading;
+            HeadingData
+        }
+    }
+}
---
+++ UIFactory/Factory/Concrete/InformationBlock/Paragraph.cs
@@ -0,0 +1,15 @@
+﻿namespace UIFactory.Factory.Concrete.InformationBlock
+{
+    public class Paragraph
+    {
+        public Infrastructure.Models.Data.InformationBlock.Paragraph paragraph { get; private set; }
+
+        private readonly Infrastructure.Models.Data.InformationBlock.Paragraph _paragraph;
+
+        public Paragraph(Infrastructure.Models.Data.InformationBlock.Paragraph paragraph)
+        {
+            _paragraph = paragraph;
+            paragraph = _paragraph;
+        }
+    }
+}
