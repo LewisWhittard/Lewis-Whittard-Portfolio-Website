e374a4dc92c599dce610aa81d0f844ddaa1a5f01
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-06-23 17:31:34 +0100

    Card_Ctor_NullAltService

--- UIFactoryTests/Concrete/CardTests.cs
+++ UIFactoryTests/Concrete/CardTests.cs
@@ -11,13 +11,13 @@ namespace UIFactoryTests.Concrete
     {
         private AltService _altService;
         private JsonLDService _jsonLDService;
-        private List<Infrastructure.Models.Data.Shared.Card.Card> _cardDatas;
+        private List<Card> _cardDatas;
 
         public void SetUp()
         {
             _altService = new AltService(new MockAltRepository());
             _jsonLDService = new JsonLDService(new MockJsonLDRepository());
-            _cardDatas = new List<Infrastructure.Models.Data.Shared.Card.Card>();
+            _cardDatas = new List<Card>();
 
             var imageDatas = new List<Infrastructure.Models.Data.Shared.Image.Image>();
             imageDatas.Add(new Infrastructure.Models.Data.Shared.Image.Image("", null, 0, false, false, "First", null, 0, null));
@@ -25,10 +25,10 @@ namespace UIFactoryTests.Concrete
             imageDatas.Add(new Infrastructure.Models.Data.Shared.Image.Image("", null, 2, false, false, "Image3GUID", null, 2, null));
             imageDatas.Add(new Infrastructure.Models.Data.Shared.Image.Image("", null, 3, false, false, "Image4GUID", null, 3, null));
 
-            _cardDatas.Add(new Infrastructure.Models.Data.Shared.Card.Card(imageDatas[0], "TitleCard0", "DescriptionCard0", "NavigationCard0", 0, false, false, 0, "First", 0));
-            _cardDatas.Add(new Infrastructure.Models.Data.Shared.Card.Card(imageDatas[1], "TitleCard1", "DescriptionCard1", "NavigationCard1", 1, false, false, 1, "Second", 1));
-            _cardDatas.Add(new Infrastructure.Models.Data.Shared.Card.Card(imageDatas[2], "TitleCard2", "DescriptionCard2", "NavigationCard2", 2, false, false, 2, "Non", 2));
-            _cardDatas.Add(new Infrastructure.Models.Data.Shared.Card.Card(imageDatas[3], "TitleCard3", "DescriptionCard3", "NavigationCard3", 3, false, false, 3, null, 3));
+            _cardDatas.Add(new Card(imageDatas[0], "TitleCard0", "DescriptionCard0", "NavigationCard0", 0, false, false, 0, "First", 0));
+            _cardDatas.Add(new Card(imageDatas[1], "TitleCard1", "DescriptionCard1", "NavigationCard1", 1, false, false, 1, "Second", 1));
+            _cardDatas.Add(new Card(imageDatas[2], "TitleCard2", "DescriptionCard2", "NavigationCard2", 2, false, false, 2, "Non", 2));
+            _cardDatas.Add(new Card(imageDatas[3], "TitleCard3", "DescriptionCard3", "NavigationCard3", 3, false, false, 3, null, 3));
         }
 
         [Theory]
@@ -96,7 +96,6 @@ namespace UIFactoryTests.Concrete
             if (Id == 0)
             {
                 Assert.Null(cardConcrete.Image.AltData);
-                Assert.Null(cardConcrete.Image.AltData);
             }
 
             switch (Id)
@@ -104,6 +103,53 @@ namespace UIFactoryTests.Concrete
                 case 0:
                     Assert.Equal("First", cardConcrete.JsonLDs[0].SuperClassGUID);
                     break;
+            }
+            TearDown();
+        }
+        
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        public void Card_Ctor_NullJsonLDService(int Id)
+        {
+            //Arrange
+            SetUp();
+            var card = _cardDatas.Where(x => x.Id == Id).FirstOrDefault();
+
+            //Act
+            var cardConcrete = new UIFactory.Factory.Concrete.Shared.Card.Card(card, _altService, null);
+
+            //Assert
+            Assert.NotNull(cardConcrete);
+            Assert.Equal(card, cardConcrete.CardData);
+            Assert.Equal(card.DisplayOrder, cardConcrete.DisplayOrder);
+            Assert.Equal(card.UIConcreteType, cardConcrete.UIConcreteType);
+            if (Id == 0)
+            {
+                Assert.Equal("First", cardConcrete.Image.AltData.SuperClassGUID);
+            }
+
+            switch (Id)
+            {
+                case 0:
+                    Assert.Null(cardConcrete.JsonLDs);
+                    break;
+                case 1:
+                    Assert.Null(cardConcrete.JsonLDs);
+                    break;
+                case 2:
+                    Assert.Null(cardConcrete.JsonLDs);
+                    break;
+                case 3:
+                    Assert.Null(cardConcrete.JsonLDs);
+                    break;
+                case 4:
+                    Assert.Null(cardConcrete.JsonLDs);
+                    break;
+
             }
             TearDown();
         }
