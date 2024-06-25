fbb66b9e1a16387a1f7d38342d96deca8528f860
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-06-25 08:25:55 +0100

    LWMD-17 Head_Ctor

--- UIFactoryTests/Concrete/HeadTests.cs
+++ UIFactoryTests/Concrete/HeadTests.cs
@@ -27,7 +27,9 @@ namespace UIFactoryTests.Concrete
         [Theory]
         [InlineData(0)]
         [InlineData(1)]
-        [InlineData(5)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
         public void Head_Ctor(int headId)
         {
             SetUp();
