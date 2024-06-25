eaf90f0b97616a517c112c08922d75e29a747722
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-06-25 20:11:01 +0100

    LWMD-17 Video Test Ammendments

--- UIFactoryTests/Concrete/VideoTests.cs
+++ UIFactoryTests/Concrete/VideoTests.cs
@@ -41,17 +41,18 @@ namespace UIFactoryTests.Concrete
             switch (videoId)
             {
                 case 0:
-                    Assert.Equal(0, videoConcrete.JsonLDDatas[0].Id);
+                    Assert.Equal("First", videoConcrete.JsonLDDatas[0].SuperClassGUID);
                     break;
                 case 1:
-                    Assert.Equal(1, videoConcrete.JsonLDDatas[0].Id);
+                    Assert.Equal("Second", videoConcrete.JsonLDDatas[0].SuperClassGUID);
                     break;
                 case 2:
-                    Assert.Equal(2, videoConcrete.JsonLDDatas[0].Id);
-                    Assert.Equal(3, videoConcrete.JsonLDDatas[1].Id);
+                    Assert.Equal("Multiple", videoConcrete.JsonLDDatas[0].SuperClassGUID);
+                    Assert.Equal("Multiple", videoConcrete.JsonLDDatas[1].SuperClassGUID);
+                    Assert.NotEqual(videoConcrete.JsonLDDatas[0], videoConcrete.JsonLDDatas[1]);
                     break;
                 case 3:
-                    Assert.Equal(0, videoConcrete.JsonLDDatas.Count());
+                    Assert.Equal(null, videoConcrete.JsonLDDatas[0].SuperClassGUID);
                     break;
                 case 4:
                     Assert.Equal(0, videoConcrete.JsonLDDatas.Count());
@@ -118,10 +119,10 @@ namespace UIFactoryTests.Concrete
             switch (videoId)
             {
                 case 0:
-                    Assert.Equal(0, videoConcrete.JsonLDDatas[0].Id);
+                    Assert.Equal("First", videoConcrete.JsonLDDatas[0].SuperClassGUID);
                     break;
                 case 1:
-                    Assert.Equal(1, videoConcrete.JsonLDDatas[0].Id);
+                    Assert.Equal("Second", videoConcrete.JsonLDDatas[0].SuperClassGUID);
                     break;
             }
             TearDown();
