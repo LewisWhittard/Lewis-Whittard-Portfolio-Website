3ac4a7a1c8f4646a052ac6cd02c6aab74c60f10d
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-06-29 13:11:40 +0100

    LMWD-17 API

--- LMWDev/Controllers/HomeController.cs
+++ LMWDev/Controllers/HomeController.cs
@@ -1,42 +1,52 @@
-﻿using Infrastructure.Repository.Page;
-using Infrastructure.Service.Page;
-using LMWDev.Models;
+﻿using LMWDev.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using SEO.Repository.AltRepository;
-using SEO.Repository.MockMetaRepository;
-using SEO.Service.AltService;
-using SEO.Service.JsonLDService;
-using SEO.Service.MetaService;
-using System.Collections.Generic;
+using System;
 using System.Diagnostics;
-using UIFactory.Factory.CSHTML;
-using UIFactory.Factory.Interface;
-using UIFactory.Strategy;
+using System.Net.Http;
+using System.Threading.Tasks;
 using UIFactory.Strategy.Interface;
 
 namespace LMWDev.Controllers
 {
-	public class HomeController : Controller
-	{
-		private readonly ILogger<HomeController> _logger;
-		private readonly IUIFactoryStrategy _uIFactoryStrategy;
-        public HomeController(ILogger<HomeController> logger)
-		{
-			_logger = logger;
-			_uIFactoryStrategy = new UIFactoryStrategy(new UIFactory.Factory.UIFactory(new PageService(new MockPageRepository()), new JsonLDService(), new AltService(new MockAltRepository()), new MetaService(new MockMetaRepository())));
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IUIFactoryStrategy _uIFactoryStrategy;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+
+        public HomeController(ILogger<HomeController> logger, IHttpClientFactory httpClientFactory)
+        {
+            _logger = logger;
+            _httpClientFactory = httpClientFactory;
         }
 
-		public IActionResult Index()
-		{
+        public async Task<IActionResult> Index()
+        {
+            // Use UIavtoryAPIController defaulttest
+            var client = _httpClientFactory.CreateClient();
+            //getlocalURl
 
-			return View(null);
-		}
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+            HttpResponseMessage response = await client.GetAsync($"{Request.Scheme}://{Request.Host}/api/UIFactoryStrategyAPI/default/test");
+            if (response.IsSuccessStatusCode)
+            {
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseBody); // Should print "Test"
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode}");
+            }
+
+            return View(null);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
 }
--- LMWDev/Startup.cs
+++ LMWDev/Startup.cs
@@ -24,10 +24,12 @@ namespace LMWDev
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddControllersWithViews();
-		}
+            services.AddHttpClient(); // Registers IHttpClientFactory
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
 			if (env.IsDevelopment())
 			{
