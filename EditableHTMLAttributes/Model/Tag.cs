0c1f7a50175e5cfda0018b2865e28315384209ae
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-07-03 22:44:35 +0100

    LWMD-18 Dependency injection

--- EditableHTMLAttributes/Model/Tag.cs
+++ EditableHTMLAttributes/Model/Tag.cs
@@ -9,6 +9,11 @@ namespace EditableHTMLAttributes.Model
         public bool Deleted { get; private set; }
         public bool Inactive { get; private set; }
 
+        private readonly int _id;
+        private readonly TagType _type;
+        private readonly bool _deleted;
+        private readonly bool _inactive;
+
         public Tag()
         {
             
@@ -16,10 +21,14 @@ namespace EditableHTMLAttributes.Model
 
         public Tag(int Id, TagType type, bool Deleted, bool Inactive)
         {
-            Id = Id;
-            Type = type;
-            Deleted = Deleted;
-            Inactive = Inactive;
+            _id = Id;
+            _type = type;
+            _deleted = Deleted;
+            _inactive = Inactive;
+            Id = _id;
+            Type = _type;
+            Deleted = _deleted;
+            Inactive = _inactive;
         }
     }
 }
