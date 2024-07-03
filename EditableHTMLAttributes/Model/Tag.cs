7024ad8a46d7c2f3e29d0474973af54d92778e55
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-07-03 22:46:03 +0100

    LMWD-18 remove di

--- EditableHTMLAttributes/Model/Tag.cs
+++ EditableHTMLAttributes/Model/Tag.cs
@@ -9,26 +9,17 @@ namespace EditableHTMLAttributes.Model
         public bool Deleted { get; private set; }
         public bool Inactive { get; private set; }
 
-        private readonly int _id;
-        private readonly TagType _type;
-        private readonly bool _deleted;
-        private readonly bool _inactive;
-
         public Tag()
         {
             
         }
 
-        public Tag(int Id, TagType type, bool Deleted, bool Inactive)
+        public Tag(int id, TagType type, bool deleted, bool inactive)
         {
-            _id = Id;
-            _type = type;
-            _deleted = Deleted;
-            _inactive = Inactive;
-            Id = _id;
-            Type = _type;
-            Deleted = _deleted;
-            Inactive = _inactive;
+            Id = id;
+            Type = type;
+            Deleted = deleted;
+            Inactive = inactive;
         }
     }
 }
