4668bd9ca8e1e05d97f926d5b947ff44d07ddf7d
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-07-03 23:37:10 +0100

    LWMD-18 Tag Repo interface and Service

---
+++ EditableHTMLAttributes/Repository/Tag/Tag/Interface/TagRepository.cs
@@ -0,0 +1,9 @@
+﻿using EditableHTMLAttributes.Model;
+
+namespace EditableHTMLAttributes.Repository.Tag.Tag.Interface
+{
+    public interface ITagRepository
+    {
+        public List<Model.Tag> GetByInformationBlockId(int id);
+    }
+}
---
+++ EditableHTMLAttributes/Service/Tag/TagService.cs
@@ -0,0 +1,26 @@
+﻿using EditableHTMLAttributes.Repository.Tag.Tag.Interface;
+
+namespace EditableHTMLAttributes.Service.Tag
+{
+    public class TagService
+    {
+        private readonly ITagRepository _tagRepository;
+
+        public TagService(ITagRepository tagRepository)
+        {
+            _tagRepository = tagRepository;
+        }
+
+        public List<Model.Tag> GetByInformationBlockId(int id, bool inactive)
+        {
+            if (inactive == false)
+            {
+                return _tagRepository.GetByInformationBlockId(id).Where(x => x.Inactive == false).ToList();
+            }
+            else
+            {
+                return _tagRepository.GetByInformationBlockId(id);
+            }
+        }
+    }
+}
