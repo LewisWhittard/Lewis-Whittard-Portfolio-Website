570bbc4903dc48e7db58f03c5b8da91929384328
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2024-07-06 10:52:44 +0100

    LWMD-18 Fix Display Order

--- EditableHTMLAttributes/Model/Tag.cs
+++ EditableHTMLAttributes/Model/Tag.cs
@@ -11,7 +11,6 @@ namespace EditableHTMLAttributes.Model
         public bool Inactive { get; private set; }
         public string UIId { get; private set; }
         public UIConcrete? UIConcreteType { get; private set; }
-        public int? DisplayerOrder { get; private set; }
         public int? DisplayOrder { get; private set; }
 
         public Tag()
