b208e7a2e227737e7dd84a26c97ba45466feabf6
LewisWhittard <63914200+LewisWhittard@users.noreply.github.com>
2025-09-04 19:05:06 +0100

    LMWD-2 | factory

---
+++ Page_Library/Page/Factory/ContentBlockFactory.cs
@@ -0,0 +1,30 @@
+﻿using Page_Library.Content.Entities.Content.Interface;
+using Page_Library.Page.Entities.ContentBlock;
+using Page_Library.Page.Entities.ContentBlock.DTO;
+using Page_Library.Page.Entities.ContentBlock.Interface;
+
+namespace Page_Library.Page.Factory
+{
+    public class ContentBlockFactory
+    {
+        public IContentBlock CreateContentBlock(ContentBlockDTO dto, IContent content)
+        {
+            switch (dto.BlockType)
+            {
+                case "Header":
+                    return new HeaderBlock(dto);
+                case "Hyperlink":
+                    return new HyperlinkBlock(dto);
+                case "Image":
+                    return new ImageBlock(dto, content);
+                case "Paragraph":
+                    return new ParagraphBlock(dto);
+                case "Video":
+                    return new VideoBlock(dto, content);
+                default:
+                    throw new ArgumentException($"Unsupported block type: {dto.BlockType}");
+            }
+
+        }
+    }
+}
