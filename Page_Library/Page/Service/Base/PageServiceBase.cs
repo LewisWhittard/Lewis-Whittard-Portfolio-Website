d1d31140c1a395714b8743efdd298f3a245f984b
LewisWhittard <63914200+LewisWhittard@users.noreply.github.com>
2025-09-08 19:24:25 +0100

    LWMD-2 | Get Page

--- Page_Library/Page/Service/Base/PageServiceBase.cs
+++ Page_Library/Page/Service/Base/PageServiceBase.cs
@@ -1,4 +1,6 @@
-﻿using Page_Library.Page.Entities.Page.Interface;
+﻿using Page_Library.Content.Repository.Interface;
+using Page_Library.Page.Entities.Page.Interface;
+using Page_Library.Page.Factory.Interface;
 using Page_Library.Page.Repository.Interface;
 using Page_Library.Page.Service.Interface;
 
@@ -7,12 +9,19 @@ namespace Page_Library.Page.Service.Base
     public abstract class PageServiceBase : IPageService
     {
         private protected readonly IPageRepository _pageRepository;
+        private protected readonly IContentBlockFactory _contentBlockFactory;
+        private protected readonly IContentRepository _contentRepository;
 
-        public PageServiceBase(IPageRepository pageRepository)
+        public PageServiceBase(IPageRepository pageRepository, IContentBlockFactory contentBlockFactory)
         {
             _pageRepository = pageRepository;
         }
 
-        public abstract IPage GetPage(int Id);
+        public IPage GetPage(int Id)
+        {
+            var results = _pageRepository.GetPage(Id);
+            results.SetUpPolymorphContentBlocks(_contentRepository, _contentBlockFactory);
+            return _pageRepository.GetPage(Id);
+        }
     }
 }
