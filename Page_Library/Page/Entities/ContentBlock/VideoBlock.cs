a1108d61a2798c392ff87fdf1ee5ab1700c6529c
LewisWhittard <63914200+LewisWhittard@users.noreply.github.com>
2025-09-15 21:02:35 +0100

    LMWD-2 | video

--- Page_Library/Page/Entities/ContentBlock/VideoBlock.cs
+++ Page_Library/Page/Entities/ContentBlock/VideoBlock.cs
@@ -1,4 +1,5 @@
-﻿using Page_Library.Content.Entities.Content.Interface;
+﻿using Page_Library.Content.Entities.Content;
+using Page_Library.Content.Entities.Content.Interface;
 using Page_Library.Page.Entities.ContentBlock.Base;
 using Page_Library.Page.Entities.ContentBlock.DTO;
 
@@ -9,9 +10,9 @@ namespace Page_Library.Page.Entities.ContentBlock
 
         public int MediaId { get; private set; }
         public string Caption { get; private set; }
-        public IContent Content { get; private set; }
+        public Video Content { get; private set; }
 
-        public VideoBlock(ContentBlockDTO dto, IContent content) : base(dto)
+        public VideoBlock(ContentBlockDTO dto, Video content) : base(dto)
         {
             MediaId = (int)dto.MediaId;
             Caption = dto.Caption;
