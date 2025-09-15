c391495a9f1148ce45fa6c1bc2627980413db71a
LewisWhittard <63914200+LewisWhittard@users.noreply.github.com>
2025-09-15 08:29:33 +0100

    LWMD-2 | Hyperlink block unit test

---
+++ Page_Library_Tests/Page/Entities/ContentBlock/HyperlinkBlock.cs
@@ -0,0 +1,32 @@
+﻿using Page_Library.Page.Entities.ContentBlock.Base;
+using Page_Library.Page.Entities.ContentBlock.DTO;
+
+namespace Page_Library_Tests.Page.Entities.ContentBlock
+{
+    public class HyperlinkBlock
+    {
+        [Fact]
+        public void Constructor_SetsPropertiesCorrectly()
+        {
+            // Arrange
+            var dto = new ContentBlockDTO
+            {
+                BlockType = "Hyperlink",
+                Alignment = "Left",
+                Url = "https://example.com",
+                LinkText = "Click here"
+            };
+
+            // Act
+            var block = new Page_Library.Page.Entities.ContentBlock.HyperlinkBlock(dto);
+
+            // Assert
+            Assert.Equal("Hyperlink", block.BlockType);
+            Assert.Equal("Left", block.Alignment);
+            Assert.Equal("https://example.com", block.Url);
+            Assert.Equal("Click here", block.LinkText);
+        }
+
+
+    }
+}
