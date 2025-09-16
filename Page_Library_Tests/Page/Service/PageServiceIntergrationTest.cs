3375a4baaf5a178462d02327aae8aa7d9e06ccfe
LewisWhittard <63914200+LewisWhittard@users.noreply.github.com>
2025-09-16 22:02:40 +0100

    LMWD-2 | Page intergration test

---
+++ Page_Library_Tests/Page/Service/PageServiceIntergrationTest.cs
@@ -0,0 +1,31 @@
+﻿using Page_Library.Content.Repository;
+using Page_Library.Page.Entities.Page.Interface;
+using Page_Library.Page.Factory;
+using Page_Library.Page.Repository;
+using Page_Library.Page.Service;
+
+namespace Page_Library_Tests.Page.Service
+{
+    public class PageServiceIntergrationTest
+    {
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void PageService_GetPageById_Correctly(int id)
+        {
+            var PagePath = Path.Combine(AppContext.BaseDirectory, "TestData", "Page", "Page.json");
+            JsonPageRepository PageRepository = new JsonPageRepository(PagePath);
+            ContentBlockFactory factory = new ContentBlockFactory();
+            var ContentPath = Path.Combine(AppContext.BaseDirectory, "TestData", "Content", "content.json");
+            JsonContentRepository contentRepository = new JsonContentRepository(ContentPath);
+
+            PageService service = new PageService(PageRepository, factory, contentRepository);
+
+            IPage result = service.GetPage(id);
+
+            Assert.Equal(id, result.ExternalId);
+        }
+
+
+    }
+}
