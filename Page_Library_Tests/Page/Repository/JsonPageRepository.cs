d3a7fa8d2bc7c2d232cc180d4e206a810da7ba39
LewisWhittard <63914200+LewisWhittard@users.noreply.github.com>
2025-09-16 08:31:09 +0100

    LMWD-2 | Page repository test

---
+++ Page_Library_Tests/Page/Repository/JsonPageRepository.cs
@@ -0,0 +1,21 @@
+﻿namespace Page_Library_Tests.Page.Repository
+{
+    public class JsonPageRepository
+    {
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void GetPage_ByID_ReturnsCorrectly(int id)
+        {
+            // Arrange
+            var jsonPath = Path.Combine(AppContext.BaseDirectory, "TestData", "Page", "Page.json");
+            var repo = new Page_Library.Page.Repository.JsonPageRepository(jsonPath);
+
+            // Act
+            var page = repo.GetPage(id);
+
+            // Assert
+            Assert.Equal(id, page.ExternalId);
+        }
+    }
+}
