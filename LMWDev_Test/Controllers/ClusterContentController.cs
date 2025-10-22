16929b57826536af0335e9829e43f1bc6883e1be
LewisWhittard <63914200+LewisWhittard@users.noreply.github.com>
2025-10-22 23:44:20 +0100

    LMWD-13 | Unit tests nename

---
+++ LMWDev_Test/Controllers/ClusterContentController.cs
@@ -0,0 +1,48 @@
+﻿using LMWDev.Controllers;
+using LMWDev.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Page_Library.Page.Entities.Page;
+using Page_Library.Page.Entities.Page.Interface;
+using Page_Library.Page.Service.Interface;
+using System.ComponentModel.DataAnnotations;
+
+namespace LMWDev_Test.Controllers
+{
+    public class ClusterContentController
+    {
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        public void ClusterContentController_ReturnIndexViewModel_Correctly(int ID)
+        {
+            // Arrange
+            var loggerMock = new Mock<ILogger<LMWDev.Controllers.ClusterContentController>>();
+            var pageServiceMock = new Mock<IPageService>();
+
+            var mockPages = new List<Mock<IPage>>
+            {
+                new Mock<IPage>(), // corresponds to ID = 1
+                new Mock<IPage>()  // corresponds to ID = 2
+            };
+
+            pageServiceMock
+                .Setup(service => service.GetPage(1))
+                .Returns(mockPages[0].Object);
+
+            pageServiceMock
+                .Setup(service => service.GetPage(2))
+                .Returns(mockPages[1].Object);
+
+            var controller = new LMWDev.Controllers.ClusterContentController(loggerMock.Object, pageServiceMock.Object);
+
+            // Act
+            var result = controller.Index(ID);
+
+            // Assert
+            var model = Assert.IsType<ClusterContentModel>(((ViewResult)result).Model);
+            Assert.Equal(mockPages[ID - 1].Object, model.Page); // Adjusted index
+        }
+    }
+}
