d3db6d2334cce4ec4e3e2b9d8adf9eb27d07ed43
LewisWhittard <63914200+LewisWhittard@users.noreply.github.com>
2026-01-14 19:10:30 +0000

    LWMD-42 first site map

---
+++ LMWDev/Controllers/SiteMapController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+[Route("sitemap.xml")]
+public class SiteMapController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        // Build the base URL dynamically (e.g., https://yourdomain.com)
+        var baseUrl = $"{Request.Scheme}://{Request.Host}";
+
+        var xml = $@"<?xml version=""1.0"" encoding=""UTF-8""?>
+<urlset xmlns=""http://www.sitemaps.org/schemas/sitemap/0.9"">
+
+  <url>
+    <loc>{baseUrl}/</loc>
+  </url>
+
+  <url>
+    <loc>{baseUrl}/search</loc>
+  </url>
+
+  <url>
+    <loc>{baseUrl}/software-development</loc>
+  </url>
+
+  <url>
+    <loc>{baseUrl}/software-development/portfolio-website-completed</loc>
+  </url>
+
+  <url>
+    <loc>{baseUrl}/software-development/my-portfolio-website-development</loc>
+  </url>
+
+  <url>
+    <loc>{baseUrl}/software-development/ui-test-automation-portfolio-piece</loc>
+  </url>
+
+  <url>
+    <loc>{baseUrl}/intersections/cogetta</loc>
+  </url>
+
+  <url>
+    <loc>{baseUrl}/software-development/from-reflection-to-action-the-marginal-gains-sprint</loc>
+  </url>
+
+  <url>
+    <loc>{baseUrl}/software-development/search-structure-and-SEO-upgrade</loc>
+  </url>
+
+  <url>
+    <loc>{baseUrl}/creative-works</loc>
+  </url>
+
+  <url>
+    <loc>{baseUrl}/creative-works/lewis-matthew-whittard-software-development-logo</loc>
+  </url>
+
+</urlset>";
+
+        return Content(xml, "application/xml", Encoding.UTF8);
+    }
+}
