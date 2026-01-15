e4ebc7005764fcece813dd753ecb6cacb46ccc21
LewisWhittard <63914200+LewisWhittard@users.noreply.github.com>
2026-01-15 23:10:24 +0000

    LMWD-43 | Cluster content unit tests

--- LMWDev_Test/Controllers/ClusterContentController.cs
+++ LMWDev_Test/Controllers/ClusterContentController.cs
@@ -13,9 +13,9 @@ namespace LMWDev_Test.Controllers
     public class ClusterContentController
     {
         [Theory]
-        [InlineData("1",0)]
-        [InlineData("2",1)]
-        public void ClusterContentController_ReturnIndexViewModel_Correctly(string id, int count)
+        [InlineData("1",0, "software-development")]
+        [InlineData("2",1, "creative-works")]
+        public void ClusterContentController_ReturnIndexViewModel_Correctly(string id, int count,string pillar)
         {
             // Arrange
             var loggerMock = new Mock<ILogger<LMWDev.Controllers.ClusterContentController>>();
@@ -26,6 +26,11 @@ namespace LMWDev_Test.Controllers
                 new Mock<IPage>(), // corresponds to ID = 1
                 new Mock<IPage>()  // corresponds to ID = 2
             };
+
+            mockPages[0].Setup(p => p.Category).Returns("Software Development");
+            mockPages[1].Setup(p => p.Category).Returns("Creative Works");
+            mockPages[0].Setup(p => p.PageType).Returns("Cluster Content Page");
+            mockPages[1].Setup(p => p.PageType).Returns("Cluster Content Page");
 
             pageServiceMock
                 .Setup(service => service.GetPage("1"))
@@ -38,11 +43,82 @@ namespace LMWDev_Test.Controllers
             var controller = new LMWDev.Controllers.ClusterContentController(loggerMock.Object, pageServiceMock.Object);
 
             // Act
-            var result = controller.Index(id);
+            var result = controller.Index(pillar,id);
 
             // Assert
             var model = Assert.IsType<ClusterContentModel>(((ViewResult)result).Model);
             Assert.Equal(mockPages[count].Object, model.Page); // Adjusted index
+        }
+
+        [Theory]
+        [InlineData("2", 0, "software-development")]
+        [InlineData("1", 1, "creative-works")]
+        public void ClusterContentController_WrongPageForPillar_404(string id, int count, string pillar)
+        {
+            // Arrange
+            var loggerMock = new Mock<ILogger<LMWDev.Controllers.ClusterContentController>>();
+            var pageServiceMock = new Mock<IPageService>();
+
+            var mockPages = new List<Mock<IPage>>
+            {
+                new Mock<IPage>(), // corresponds to ID = 1
+                new Mock<IPage>()  // corresponds to ID = 2
+            };
+
+            mockPages[0].Setup(p => p.Category).Returns("Software Development");
+            mockPages[1].Setup(p => p.Category).Returns("Creative Works");
+            mockPages[0].Setup(p => p.PageType).Returns("Cluster Content Page");
+            mockPages[1].Setup(p => p.PageType).Returns("Cluster Content Page");
+
+            pageServiceMock
+                .Setup(service => service.GetPage("1"))
+                .Returns(mockPages[0].Object);
+
+            pageServiceMock
+                .Setup(service => service.GetPage("2"))
+                .Returns(mockPages[1].Object);
+
+            var controller = new LMWDev.Controllers.ClusterContentController(loggerMock.Object, pageServiceMock.Object);
+
+            // Act
+            var result = controller.Index(pillar, id);
+
+            // Assert
+            var notFound = Assert.IsType<NotFoundResult>(result);
+            Assert.Equal(404, notFound.StatusCode);
+
+        }
+
+        [Theory]
+        [InlineData("1", 0, "software-development")]
+        public void ClusterContentController_WrongPageType_404(string id, int count, string pillar)
+        {
+            // Arrange
+            var loggerMock = new Mock<ILogger<LMWDev.Controllers.ClusterContentController>>();
+            var pageServiceMock = new Mock<IPageService>();
+
+            var mockPages = new List<Mock<IPage>>
+            {
+                new Mock<IPage>(), // corresponds to ID = 1
+            };
+
+            mockPages[0].Setup(p => p.Category).Returns("Software Development");
+            mockPages[0].Setup(p => p.PageType).Returns("Pillar Page");
+
+
+            pageServiceMock
+                .Setup(service => service.GetPage("1"))
+                .Returns(mockPages[0].Object);
+
+            var controller = new LMWDev.Controllers.ClusterContentController(loggerMock.Object, pageServiceMock.Object);
+
+            // Act
+            var result = controller.Index(pillar, id);
+
+            // Assert
+            var notFound = Assert.IsType<NotFoundResult>(result);
+            Assert.Equal(404, notFound.StatusCode);
+
         }
     }
 }
