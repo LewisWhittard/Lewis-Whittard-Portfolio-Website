9a9eeba45aeba25be79adbf1c996d2f998439728
LewisWhittard <63914200+LewisWhittard@users.noreply.github.com>
2026-01-20 19:46:28 +0000

    LMWD-45 | Pillar page standard original test fix

--- LMWDev_Test/Controllers/PillarPageController.cs
+++ LMWDev_Test/Controllers/PillarPageController.cs
@@ -11,49 +11,44 @@ namespace LMWDev_Test.Controllers
     public class PillarPageController
     {
         [Theory]
-        [InlineData("1", 0)]
-        [InlineData("2", 1)]
-        public void PillarPage_Controller_ReturnIndexViewModel_Correctly(string id, int count)
+        [InlineData("software-development", 0)]
+        [InlineData("creative-works", 1)]
+        public void Index_ReturnsViewModel_ForValidRoutes(string id, int index)
         {
             // Arrange
             var loggerMock = new Mock<ILogger<LMWDev.Controllers.PillarPageController>>();
             var pageServiceMock = new Mock<IPageService>();
 
             var mockPages = new List<IPage>
-            {
-                Mock.Of<IPage>(p => p.Category == "Category1"),
-                Mock.Of<IPage>(p => p.Category == "Category2")
-            };
+    {
+        Mock.Of<IPage>(p => p.Category == "Category1"),
+        Mock.Of<IPage>(p => p.Category == "Category2")
+    };
 
-            var mockSearchReusltsOne = new List<ISearchResult>
-            {
-                new Mock<ISearchResult>().Object,
-                new Mock<ISearchResult>().Object
-            };
+            var mockSearchResultsOne = new List<ISearchResult>
+    {
+        new Mock<ISearchResult>().Object,
+        new Mock<ISearchResult>().Object
+    };
 
-            var mockSearchReusltsTwo = new List<ISearchResult>
-            {
-                new Mock<ISearchResult>().Object,
-                new Mock<ISearchResult>().Object,
-                new Mock<ISearchResult>().Object
-            };
+            var mockSearchResultsTwo = new List<ISearchResult>
+    {
+        new Mock<ISearchResult>().Object,
+        new Mock<ISearchResult>().Object,
+        new Mock<ISearchResult>().Object
+    };
 
-            pageServiceMock
-                .Setup(service => service.GetPage("1"))
-                .Returns(mockPages[0]);
+            pageServiceMock.Setup(s => s.GetPage("software-development"))
+                           .Returns(mockPages[0]);
 
-            pageServiceMock
-                .Setup(service => service.GetPage("2"))
-                .Returns(mockPages[1]);
+            pageServiceMock.Setup(s => s.GetPage("creative-works"))
+                           .Returns(mockPages[1]);
 
-            pageServiceMock
-                .Setup(service => service.Search(null, "Category1"))
-                .Returns(mockSearchReusltsOne);
+            pageServiceMock.Setup(s => s.Search(null, "Category1"))
+                           .Returns(mockSearchResultsOne);
 
-            pageServiceMock
-                .Setup(service => service.Search(null, "Category2"))
-                .Returns(mockSearchReusltsTwo);
-
+            pageServiceMock.Setup(s => s.Search(null, "Category2"))
+                           .Returns(mockSearchResultsTwo);
 
             var controller = new LMWDev.Controllers.PillarPageController(loggerMock.Object, pageServiceMock.Object);
 
@@ -62,18 +57,12 @@ namespace LMWDev_Test.Controllers
 
             // Assert
             var model = Assert.IsType<PillarPageModel>(((ViewResult)result).Model);
-            Assert.Equal(mockPages[count], model.Page); // Adjusted index
-            Assert.Equal(mockPages[count], model.Page); // Adjusted index
+            Assert.Equal(mockPages[index], model.Page);
 
-            if (id == "1")
-            {
-                Assert.Equal(mockSearchReusltsOne, model.Results);
-
-            }
+            if (id == "software-development")
+                Assert.Equal(mockSearchResultsOne, model.Results);
             else
-            {
-                Assert.Equal(mockSearchReusltsTwo, model.Results);
-            }
+                Assert.Equal(mockSearchResultsTwo, model.Results);
         }
     }
 }
