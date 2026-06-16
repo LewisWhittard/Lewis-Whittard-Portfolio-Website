6e0dc0bb1237b29591a2f9b6071dcf66f01b6ef9
LewisWhittard <63914200+LewisWhittard@users.noreply.github.com>
2026-06-16 08:33:14 +0100

    LMWD-96 | id replacements

--- Page_Library_Tests/Page/Entities/ContentBlock/ImageBlock.cs
+++ Page_Library_Tests/Page/Entities/ContentBlock/ImageBlock.cs
@@ -1,8 +1,5 @@
-﻿using Moq;
 using Page_Library.Page.Entities.ContentBlock.DTO;
 using Page_Library.Page.Entities.ContentBlock;
-using Page_Library.Content.Entities.Content.DTO;
-using Page_Library.Content.Entities.Content;
 
 namespace Page_Library_Tests.Page.Entities.ContentBlock
 {
@@ -16,31 +13,18 @@ namespace Page_Library_Tests.Page.Entities.ContentBlock
             {
                 BlockType = "Image",
                 Alignment = "Center",
-                MediaId = 42
-            };
-
-            // Arrange
-            var contentDTO = new contentDTO
-            {
-                ID = 1,
-                Name = "Test Name",
-                Path = "/test/path.jpg",
-                Alt = "Test Alt",
-                ContentType = "Image"
+                ImageUrl = "/test/path.jpg",
+                Alt = "Test Alt"
             };
 
             // Act
-            var image = new Image(contentDTO);
-
-            // Act
-            var block = new ImageBlock(dto, image);
+            var block = new ImageBlock(dto);
 
             // Assert
             Assert.Equal("Image", block.BlockType);
             Assert.Equal("Center", block.Alignment);
-            Assert.Equal(42, block.MediaId);
-            Assert.Equal(image, block.Content);
+            Assert.Equal("/test/path.jpg", block.URL);
+            Assert.Equal("Test Alt", block.Alt);
         }
     }
 }
-
